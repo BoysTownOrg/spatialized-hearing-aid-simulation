3c7f0f6aeab338de8cf8345dbcff77f5d5b68eea
Bashford, Seth <seth.bashford@boystown.org>
2018-10-23 13:06:50 -0500

    fail

--- src/google-tests/HearingAidProcessorTests.cpp
+++ src/google-tests/HearingAidProcessorTests.cpp
@@ -2,6 +2,18 @@
 
 class HearingAidProcessorTestCase : public ::testing::TestCase {};
 
-TEST(HearingAidProcessorTestCase, tbd) {
-
+TEST(
+	HearingAidProcessorTestCase, 
+	processCallsFilterbankCompressorMethodsInCorrectOrder) 
+{
+	const auto compressor = std::make_shared<MockFilterbankCompressor>();
+	HearingAidProcessor processor{ compressor };
+	processor.process();
+	EXPECT_EQ(
+		"compressInput "
+		"analyzeFilterbank "
+		"compressChannels "
+		"synthesizeFilterbank "
+		"compressOutput ",
+		compressor->processingLog());
 }
