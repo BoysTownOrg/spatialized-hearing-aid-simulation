c2c2254ad9da061f86eb20865e249d2d6f61ca9f
Bashford, Seth <seth.bashford@boystown.org>
2018-10-25 13:28:03 -0500

    pass

--- src/google-tests/AudioDeviceControllerTests.cpp
+++ src/google-tests/AudioDeviceControllerTests.cpp
@@ -1,4 +1,44 @@
+#include <common-includes/Interface.h>
+
+class AudioDeviceController;
+
+class AudioDevice {
+public:
+	INTERFACE_OPERATIONS(AudioDevice);
+	virtual void setController(AudioDeviceController *) = 0;
+};
+
+class AudioStream {};
+
+#include <memory>
+
+class AudioDeviceController {
+public:
+	AudioDeviceController(
+		std::shared_ptr<AudioDevice> device,
+		std::shared_ptr<AudioStream>
+	) 
+	{
+		device->setController(this);
+	}
+};
+
 #include <gtest/gtest.h>
+
+class MockAudioDevice : public AudioDevice {
+	AudioDeviceController *_controller{};
+public:
+	const AudioDeviceController *controller() const {
+		return _controller;
+	}
+	void setController(AudioDeviceController *c) override {
+		_controller = c;
+	}
+};
+
+class MockAudioStream : public AudioStream {
+
+};
 
 class AudioDeviceControllerTestCase : public ::testing::TestCase {};
 
