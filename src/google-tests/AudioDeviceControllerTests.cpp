b17404c1a37b1ca17c28b36c18701a7ab6d02076
Bashford, Seth <seth.bashford@boystown.org>
2018-10-25 13:23:06 -0500

    fail

--- src/google-tests/AudioDeviceControllerTests.cpp
+++ src/google-tests/AudioDeviceControllerTests.cpp
@@ -2,6 +2,9 @@
 
 class AudioDeviceControllerTestCase : public ::testing::TestCase {};
 
-TEST(AudioDeviceControllerTestCase, tbd) {
-
+TEST(AudioDeviceControllerTestCase, constructorSetsItself) {
+	const auto device = std::make_shared<MockAudioDevice>();
+	const auto stream = std::make_shared<MockAudioStream>();
+	AudioDeviceController controller{ device, stream };
+	EXPECT_EQ(&controller, device->controller());
 }
