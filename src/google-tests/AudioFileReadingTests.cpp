6ea7fe15284532c1507b0c2a805fa15c184f198d
Bashford, Seth <seth.bashford@boystown.org>
2018-10-25 15:04:47 -0500

    fail

--- src/google-tests/AudioFileReadingTests.cpp
+++ src/google-tests/AudioFileReadingTests.cpp
@@ -3,5 +3,8 @@
 class AudioFileReadingTestCase : public ::testing::TestCase {};
 
 TEST(AudioFileReadingTestCase, tbd) {
-
+	const auto reader = std::make_shared<MockAudioFileReader>({ 3, 4, 5, 6 });
+	reader->setChannels(2);
+	AudioFileInMemory audioFile{ reader };
+	EXPECT_EQ({ 3 }, audioFile.readLeftChannel(1));
 }
