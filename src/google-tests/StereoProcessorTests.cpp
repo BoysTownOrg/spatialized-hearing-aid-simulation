e6b3d1d542e390ad819224d01841b88b33d526be
Bashford, Seth <seth.bashford@boystown.org>
2018-10-26 11:37:17 -0500

    fail

--- src/google-tests/StereoProcessorTests.cpp
+++ src/google-tests/StereoProcessorTests.cpp
@@ -3,5 +3,14 @@
 class StereoProcessorTestCase : public ::testing::TestCase {};
 
 TEST(StereoProcessorTestCase, tbd) {
-
+	const auto left = std::make_shared<MockMonoProcessor>();
+	const auto right = std::make_shared<MockMonoProcessor>();
+	StereoProcessor stereo{ left, right };
+	float x{};
+	float y{};
+	stereo.process(&x, &y, 1);
+	EXPECT_EQ(&x, left->signal());
+	EXPECT_EQ(&y, right->signal());
+	EXPECT_EQ(1, left->frames());
+	EXPECT_EQ(1, right->frames());
 }
