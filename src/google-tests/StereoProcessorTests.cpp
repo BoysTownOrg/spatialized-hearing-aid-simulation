c82a355e444d9889cabc836bb1e213bb3d69efa7
Bashford, Seth <seth.bashford@boystown.org>
2018-10-26 11:42:20 -0500

    pass

--- src/google-tests/StereoProcessorTests.cpp
+++ src/google-tests/StereoProcessorTests.cpp
@@ -1,15 +1,27 @@
-class MonoProcessor {};
+#include <common-includes/Interface.h>
+
+class MonoProcessor {
+public:
+	INTERFACE_OPERATIONS(MonoProcessor);
+	virtual void process(float *, int) = 0;
+};
 
 #include <memory>
 
 class StereoProcessor {
+	std::shared_ptr<MonoProcessor> left;
+	std::shared_ptr<MonoProcessor> right;
 public:
 	StereoProcessor(
 		std::shared_ptr<MonoProcessor> left,
 		std::shared_ptr<MonoProcessor> right
-	) {}
-	void process(float *, float *, int ) {
+	) :
+		left{ std::move(left) },
+		right{ std::move(right) } {}
 
+	void process(float *xLeft, float *xRight, int frameCount) {
+		left->process(xLeft, frameCount);
+		right->process(xRight, frameCount);
 	}
 };
 
@@ -24,6 +36,10 @@ public:
 	}
 	int frames() const {
 		return _frameCount;
+	}
+	virtual void process(float *x, int count) override {
+		_signal = x;
+		_frameCount = count;
 	}
 };
 
