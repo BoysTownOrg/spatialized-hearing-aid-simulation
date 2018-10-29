314fbd4a570b957f9babda144a86c9bd55b00811
Bashford, Seth <seth.bashford@boystown.org>
2018-10-29 14:01:36 -0500

    pass

--- src/google-tests/StereoAudioFileAdapterTests.cpp
+++ src/google-tests/StereoAudioFileAdapterTests.cpp
@@ -1,4 +1,48 @@
+#include <common-includes/Interface.h>
+
+class StereoAudioFile {
+public:
+	INTERFACE_OPERATIONS(StereoAudioFile);
+	virtual void read(float *left, float *right, int samples) = 0;
+};
+
+#include <audio-stream-processing/AudioReader.h>
+#include <memory>
+
+class StereoAudioFileAdapter : public AudioReader {
+	std::shared_ptr<StereoAudioFile> file;
+public:
+	explicit StereoAudioFileAdapter(
+		std::shared_ptr<StereoAudioFile> file
+	) :
+		file{ std::move(file) } {}
+	void read(float **channels, int frameCount) override {
+		file->read(channels[0], channels[1], frameCount);
+	}
+};
+
 #include <gtest/gtest.h>
+
+class MockStereoAudioFile : public StereoAudioFile {
+	float *_left{};
+	float *_right{};
+	int _samples{};
+public:
+	const float *left() const {
+		return _left;
+	}
+	const float *right() const {
+		return _right;
+	}
+	int samples() const {
+		return _samples;
+	}
+	void read(float * left, float * right, int samples) override {
+		_left = left;
+		_right = right;
+		_samples = samples;
+	}
+};
 
 class StereoAudioFileAdapterTestCase : public ::testing::TestCase {};
 
@@ -11,5 +55,5 @@ TEST(StereoAudioFileAdapterTestCase, tbd) {
 	adapter.read(channels, 1);
 	EXPECT_EQ(&left, file->left());
 	EXPECT_EQ(&right, file->right());
-	EXPECT_EQ(1, file->frameCount());
+	EXPECT_EQ(1, file->samples());
 }
