a03cc1b41d5c94bc2d2ddd1002cac86fab18112c
Bashford, Seth <seth.bashford@boystown.org>
2018-10-29 14:26:32 -0500

    refactor

--- src/signal-processing/StereoProcessorAdapter.h
+++ src/signal-processing/StereoProcessorAdapter.h
@@ -1,26 +1,15 @@
 #pragma once
 
-#include <common-includes/Interface.h>
-
-class StereoProcessor {
-public:
-	INTERFACE_OPERATIONS(StereoProcessor);
-	virtual void process(float *xLeft, float *xRight, int samples) = 0;
-};
-
+#include "signal-processing-exports.h"
+#include "StereoProcessor.h"
 #include <audio-stream-processing/AudioProcessor.h>
 #include <memory>
 
 class StereoProcessorAdapter : public AudioProcessor {
 	std::shared_ptr<StereoProcessor> processor;
 public:
-	explicit StereoProcessorAdapter(
-		std::shared_ptr<StereoProcessor> processor
-	) :
-		processor{ std::move(processor) } {}
-
-	void process(float **channels, int frameCount) override {
-		processor->process(channels[0], channels[1], frameCount);
-	}
+	SIGNAL_PROCESSING_API explicit StereoProcessorAdapter(
+		std::shared_ptr<StereoProcessor> processor);
+	SIGNAL_PROCESSING_API void process(float **channels, int frameCount) override;
 };
 
