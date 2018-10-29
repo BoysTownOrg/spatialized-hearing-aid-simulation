197e490dd795235323f751a817a9b71cc8a63ee9
Bashford, Seth <seth.bashford@boystown.org>
2018-10-29 10:00:06 -0500

    fail

--- src/google-tests/MonoProcessingChainTests.cpp
+++ src/google-tests/MonoProcessingChainTests.cpp
@@ -3,5 +3,11 @@
 class MonoProcessingChainTestCase : public ::testing::TestCase {};
 
 TEST(MonoProcessingChainTestCase, tbd) {
-
+	const auto first = std::make_shared<MockMonoProcessor>();
+	const auto second = std::make_shared<MockMonoProcessor>();
+	MonoProcessingChain chain{};
+	chain.add(first);
+	chain.add(second);
+	chain.process(nullptr, 0);
+	assertFirstCalledThenSecond();
 }
