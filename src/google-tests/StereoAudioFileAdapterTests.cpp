41dbcb7e2d21f8d9ed9d8997643b1fcdcf2fe08f
Bashford, Seth <seth.bashford@boystown.org>
2018-10-29 13:56:03 -0500

    fail

--- src/google-tests/StereoAudioFileAdapterTests.cpp
+++ src/google-tests/StereoAudioFileAdapterTests.cpp
@@ -3,5 +3,13 @@
 class StereoAudioFileAdapterTestCase : public ::testing::TestCase {};
 
 TEST(StereoAudioFileAdapterTestCase, tbd) {
-
+	const auto file = std::make_shared<MockStereoAudioFile>();
+	StereoAudioFileAdapter adapter{ file };
+	float left{};
+	float right{};
+	float *channels[] = { &left, &right };
+	adapter.read(channels, 1);
+	EXPECT_EQ(&left, file->left());
+	EXPECT_EQ(&right, file->right());
+	EXPECT_EQ(1, file->frameCount());
 }
