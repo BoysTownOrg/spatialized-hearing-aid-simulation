02fa6b0266cf24618121af79cc29a9e827b30cfa
Bashford, Seth <seth.bashford@boystown.org>
2018-10-29 14:16:40 -0500

    fail

--- src/google-tests/StereoProcessorAdapterTests.cpp
+++ src/google-tests/StereoProcessorAdapterTests.cpp
@@ -3,5 +3,13 @@
 class StereoProcessorAdapterTestCase : public ::testing::TestCase {};
 
 TEST(StereoProcessorAdapterTestCase, tbd) {
-
+	const auto processor = std::make_shared<MockStereoProcessor>();
+	StereoProcessorAdapter adapter{ processor };
+	float left{};
+	float right{};
+	float *channels[] = { &left, &right };
+	adapter.process(channels, 1);
+	EXPECT_EQ(&left, processor->left());
+	EXPECT_EQ(&right, processor->right());
+	EXPECT_EQ(1, processor->samples());
 }
