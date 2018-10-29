0f91624911a2ed32e4134d97b3375bff2482fabd
Bashford, Seth <seth.bashford@boystown.org>
2018-10-29 14:21:36 -0500

    pass

--- src/google-tests/StereoProcessorAdapterTests.cpp
+++ src/google-tests/StereoProcessorAdapterTests.cpp
@@ -1,4 +1,49 @@
+#include <common-includes/Interface.h>
+
+class StereoProcessor {
+public:
+	INTERFACE_OPERATIONS(StereoProcessor);
+	virtual void process(float *xLeft, float *xRight, int samples) = 0;
+};
+
+#include <audio-stream-processing/AudioProcessor.h>
+#include <memory>
+
+class StereoProcessorAdapter : public AudioProcessor {
+	std::shared_ptr<StereoProcessor> processor;
+public:
+	explicit StereoProcessorAdapter(
+		std::shared_ptr<StereoProcessor> processor
+	) :
+		processor{ std::move(processor) } {}
+
+	void process(float **channels, int frameCount) override {
+		processor->process(channels[0], channels[1], frameCount);
+	}
+};
+
 #include <gtest/gtest.h>
+
+class MockStereoProcessor : public StereoProcessor {
+	float *_left{};
+	float *_right{};
+	int _samples{};
+public:
+	const float *left() const {
+		return _left;
+	}
+	const float *right() const {
+		return _right;
+	}
+	int samples() const {
+		return _samples;
+	}
+	void process(float * xLeft, float * xRight, int samples) override {
+		_left = xLeft;
+		_right = xRight;
+		_samples = samples;
+	}
+};
 
 class StereoProcessorAdapterTestCase : public ::testing::TestCase {};
 
