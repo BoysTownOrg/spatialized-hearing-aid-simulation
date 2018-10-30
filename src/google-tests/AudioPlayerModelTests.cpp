12e19b4c10e6505f194ac5e61cdb4bd1706c73a9
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 10:10:23 -0500

    pass

--- src/google-tests/AudioPlayerModelTests.cpp
+++ src/google-tests/AudioPlayerModelTests.cpp
@@ -1,3 +1,11 @@
+#include <presentation/Model.h>
+
+class AudioPlayerModel : public Model {
+public:
+	void playRequest(PlayRequest) override {
+	}
+};
+
 #include <gtest/gtest.h>
 
 class AudioPlayerModelTestCase : public ::testing::TestCase {};
