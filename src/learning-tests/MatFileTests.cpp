5250055f180206e1ceb927a26a556c25f7376e5f
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 16:19:51 -0500

    well i'll be

--- src/learning-tests/MatFileTests.cpp
+++ src/learning-tests/MatFileTests.cpp
@@ -3,7 +3,13 @@
 
 class MatFileTestCase : public ::testing::TestCase {};
 
-TEST(MatFileTestCase, tbd) {
+TEST(MatFileTestCase, canOpen) {
 	const auto matFile = matOpen("../example.mat", "r");
 	EXPECT_FALSE(matFile == nullptr);
+}
+
+TEST(MatFileTestCase, tbd) {
+	const auto matFile = matOpen("../example.mat", "r");
+	const auto s = matGetVariable(matFile, "s");
+	EXPECT_FALSE(s == nullptr);
 }
