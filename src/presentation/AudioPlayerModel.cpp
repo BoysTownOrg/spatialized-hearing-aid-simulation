fc601c359633b703a99d4a160fe83afd76d83f72
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 10:47:35 -0500

    pass

--- src/presentation/AudioPlayerModel.cpp
+++ src/presentation/AudioPlayerModel.cpp
@@ -8,4 +8,11 @@ void AudioPlayerModel::playRequest(PlayRequest request) {
 		throw RequestFailure{ 
 			"'" + request.level_dB_Spl + "' is not a valid level..." };
 	}
+	try {
+		std::stod(request.attack_ms);
+	}
+	catch (const std::invalid_argument &) {
+		throw RequestFailure{
+			"'" + request.attack_ms + "' is not a valid attack time." };
+	}
 }
