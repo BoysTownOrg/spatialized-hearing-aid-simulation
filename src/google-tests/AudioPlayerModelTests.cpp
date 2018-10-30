1c0955cf0697b601b8083d002149cc0d81c20906
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 10:26:27 -0500

    changed test, fail

--- src/google-tests/AudioPlayerModelTests.cpp
+++ src/google-tests/AudioPlayerModelTests.cpp
@@ -3,19 +3,23 @@
 
 class AudioPlayerModelTestCase : public ::testing::TestCase {};
 
-TEST(AudioPlayerModelTestCase, badParametersReturnErrorMessages) {
+TEST(AudioPlayerModelTestCase, badParametersThrowErrors) {
 	AudioPlayerModel model{};
-	const auto response = model.playRequest(
-		{
-			"",
-			"",
-			"",
-			"",
-			"a",
-			"2.0",
-			"3.0",
-			"4",
-			"5"
-		});
-	EXPECT_EQ(Model::Status::fail, response.status);
+	try {
+		model.playRequest(
+			{
+				"",
+				"",
+				"",
+				"",
+				"a",
+				"2.0",
+				"3.0",
+				"4",
+				"5"
+			});
+	}
+	catch (const Model::RequestFailure &failure) {
+		EXPECT_EQ("'a' is not a valid level...", failure.what());
+	}
 }
