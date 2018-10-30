e7cdb9589f829daae46853be26d2751c8e604544
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 10:46:49 -0500

    fail

--- src/google-tests/AudioPlayerModelTests.cpp
+++ src/google-tests/AudioPlayerModelTests.cpp
@@ -24,3 +24,25 @@ TEST(AudioPlayerModelTestCase, badLevelThrowsRequestFailure) {
 		EXPECT_EQ(std::string{ "'a' is not a valid level..." }, failure.what());
 	}
 }
+
+TEST(AudioPlayerModelTestCase, badAttackThrowsRequestFailure) {
+	try {
+		AudioPlayerModel model{};
+		model.playRequest(
+			{
+				"",
+				"",
+				"",
+				"",
+				"1.0",
+				"b",
+				"3.0",
+				"4",
+				"5"
+			});
+		FAIL() << "Expected Model::RequestFailure";
+	}
+	catch (const Model::RequestFailure &failure) {
+		EXPECT_EQ(std::string{ "'b' is not a valid attack time." }, failure.what());
+	}
+}
