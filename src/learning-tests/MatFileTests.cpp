621990c389509f66c8979c9b159d4f243fdfc66a
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 16:05:52 -0500

    so I had to copy the entire runtime and use x64...not looking good

--- src/learning-tests/MatFileTests.cpp
+++ src/learning-tests/MatFileTests.cpp
@@ -1,7 +1,9 @@
+#include <mat.h>
 #include <gtest/gtest.h>
 
 class MatFileTestCase : public ::testing::TestCase {};
 
 TEST(MatFileTestCase, tbd) {
-
+	const auto matFile = matOpen("../example.mat", "r");
+	EXPECT_FALSE(matFile == nullptr);
 }
