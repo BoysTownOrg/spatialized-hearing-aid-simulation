c3d0ad9fbfc884de35a5f112d2118ca7961c18d0
Bashford, Seth <seth.bashford@boystown.org>
2018-10-30 10:00:30 -0500

    adding new overrides

--- src/main/FltkWindow.cpp
+++ src/main/FltkWindow.cpp
@@ -1,8 +1,12 @@
 #include "FltkWindow.h"
 #include <sstream>
 
-void FltkWindow::onBrowsePrescription(Fl_Widget *, void *self) {
+void FltkWindow::onBrowseLeftPrescription(Fl_Widget *, void *self) {
 	static_cast<FltkWindow *>(self)->presenter->browseForLeftDslPrescription();
+}
+
+void FltkWindow::onBrowseRightPrescription(Fl_Widget *, void *self) {
+	static_cast<FltkWindow *>(self)->presenter->browseForRightDslPrescription();
 }
 
 void FltkWindow::onBrowseAudio(Fl_Widget *, void *self) {
@@ -14,17 +18,25 @@ void FltkWindow::onBrowseBrir(Fl_Widget *, void *self) {
 }
 
 FltkWindow::FltkWindow() :
-	window(500, 300, 450, 300),
-	_prescriptionFilePath(150, 50, 200, 40, "Left DSL prescription file path"),
-	_audioFilePath(150, 100, 200, 40, "Audio file path"),
-	_brirFilePath(150, 150, 200, 40, "BRIR file path"),
-	browsePrescription(360, 50, 60, 40, "Browse"),
-	browseAudio(360, 100, 60, 40, "Browse"),
-	browseBrir(360, 150, 60, 40, "Browse")
+	window(500, 300, 450, 500),
+	_leftPrescriptionFilePath(150, 50, 200, 40, "Left DSL prescription file path"),
+	_rightPrescriptionFilePath(150, 100, 200, 40, "Right DSL prescription file path"),
+	_audioFilePath(150, 150, 200, 40, "Audio file path"),
+	_brirFilePath(150, 200, 200, 40, "BRIR file path"),
+	browseLeftPrescription(360, 50, 60, 40, "Browse"),
+	browseRightPrescription(360, 100, 60, 40, "Browse"),
+	browseAudio(360, 150, 60, 40, "Browse"),
+	browseBrir(360, 200, 60, 40, "Browse"),
+	_level_dB_Spl(150, 250, 200, 40, "Level (dB SPL)"),
+	_attack_ms(150, 300, 200, 40, "attack (ms)"),
+	_release_ms(150, 350, 200, 40, "release (ms)"),
+	_windowSize(150, 400, 200, 40, "window size (samples)"),
+	_chunkSize(150, 450, 200, 40, "chunk size (samples)")
 {
 	window.end();
 	window.show();
-	browsePrescription.callback(onBrowsePrescription, this);
+	browseLeftPrescription.callback(onBrowseLeftPrescription, this);
+	browseRightPrescription.callback(onBrowseRightPrescription, this);
 	browseAudio.callback(onBrowseAudio, this);
 	browseBrir.callback(onBrowseBrir, this);
 }
@@ -61,7 +73,11 @@ bool FltkWindow::browseCancelled() {
 }
 
 void FltkWindow::setLeftDslPrescriptionFilePath(std::string p) {
-	_prescriptionFilePath.value(p.c_str());
+	_leftPrescriptionFilePath.value(p.c_str());
+}
+
+void FltkWindow::setRightDslPrescriptionFilePath(std::string p) {
+	_rightPrescriptionFilePath.value(p.c_str());
 }
 
 void FltkWindow::setAudioFilePath(std::string p) {
@@ -73,7 +89,7 @@ void FltkWindow::setBrirFilePath(std::string p) {
 }
 
 std::string FltkWindow::leftDslPrescriptionFilePath() const {
-	return _prescriptionFilePath.value();
+	return _leftPrescriptionFilePath.value();
 }
 
 std::string FltkWindow::audioFilePath() const {
@@ -82,4 +98,34 @@ std::string FltkWindow::audioFilePath() const {
 
 std::string FltkWindow::brirFilePath() const {
 	return _brirFilePath.value();
+}
+
+std::string FltkWindow::rightDslPrescriptionFilePath() const
+{
+	return _rightPrescriptionFilePath.value();
+}
+
+std::string FltkWindow::level_dB_Spl() const
+{
+	return _level_dB_Spl.value();
+}
+
+std::string FltkWindow::attack_ms() const
+{
+	return _attack_ms.value();
+}
+
+std::string FltkWindow::release_ms() const
+{
+	return _release_ms.value();
+}
+
+std::string FltkWindow::windowSize() const
+{
+	return _windowSize.value();
+}
+
+std::string FltkWindow::chunkSize() const
+{
+	return _chunkSize.value();
 }
