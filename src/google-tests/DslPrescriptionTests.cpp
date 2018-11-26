b851345f00d7df118653fc24ccc45a355bfd02cf
Bashford, Seth <seth.bashford@boystown.org>
2018-11-26 11:34:08 -0600

    compile fail

--- src/google-tests/DslPrescriptionTests.cpp
+++ src/google-tests/DslPrescriptionTests.cpp
@@ -2,6 +2,28 @@
 
 class DslPrescriptionTestCase : public ::testing::TestCase {};
 
-TEST(DslPrescriptionTestCase, tbd) {
-	FAIL();
+static void assertInvalidPrescriptionThrownOnChannelCountMismatch(
+	std::string property)
+{
+	try {
+		MockDslPrescriptionParser parser{};
+		parser.setValidSingleChannelProperties();
+		parser.setVectorProperty(property, {});
+		DslPrescription{ parser };
+		FAIL() << "Expected DslPrescription::InvalidPrescription.";
+	}
+	catch (const DslPrescription::InvalidPrescription &e) {
+		EXPECT_EQ("channel count mismatch in prescription.", e.what());
+	}
+}
+
+TEST(DslPrescriptionTestCase, constructorThrowsInvalidPrescriptionOnChannelCountMismatches) {
+	for (const std::string property :
+		{
+			"compression_ratios",
+			"kneepoint_gains_dB",
+			"kneepoints_dB_SPL",
+			"BOLT_dB_SPL"
+		})
+		assertInvalidPrescriptionThrownOnChannelCountMismatch(property);
 }
