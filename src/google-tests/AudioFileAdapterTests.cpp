f0d9972e1e296a9d6862ddcc5bfbec5e75746940
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 11:48:22 -0600

    pass

--- src/google-tests/AudioFileAdapterTests.cpp
+++ src/google-tests/AudioFileAdapterTests.cpp
@@ -1,13 +1,27 @@
 #include <audio-file-reading/AudioFileReader.h>
 #include <audio-stream-processing/AudioFrameReader.h>
+#include <gsl/gsl>
+#include <vector>
 
 class AudioFileAdapter : public AudioFrameReader {
-	std::shared_ptr<AudioFileReader> reader;
+	using size_type = std::vector<float>::size_type;
+	std::vector<float> buffer;
+	int channelCount;
+	size_type head = 0;
 public:
-	explicit AudioFileAdapter(std::shared_ptr<AudioFileReader> reader) : reader{ std::move(reader) } {}
+	explicit AudioFileAdapter(
+		std::shared_ptr<AudioFileReader> reader
+	) :
+		buffer(gsl::narrow<size_type>(reader->frames() * reader->channels())),
+		channelCount(reader->channels())
+	{
+		reader->readFrames(&buffer[0], reader->frames());
+	}
+
 	void read(float ** channels, int frameCount) override {
-		channels;
-		frameCount;
+		for (int i = 0; i < frameCount; ++i)
+			for (int j = 0; j < channelCount; ++j)
+				channels[j][i] = buffer[head++];
 	}
 };
 
@@ -22,9 +36,9 @@ TEST(AudioFileAdapterTestCase, tbd) {
 		std::vector<float>{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
 	reader->setChannels(3);
 	AudioFileAdapter adapter{ reader };
-	std::vector<float> a(3);
-	std::vector<float> b(3);
-	std::vector<float> c(3);
+	std::vector<float> a(4);
+	std::vector<float> b(4);
+	std::vector<float> c(4);
 	float *channels[] { &a[0], &b[0], &c[0] };
 	adapter.read(channels, 4);
 	assertEqual({ 1, 4, 7, 10 }, a);
