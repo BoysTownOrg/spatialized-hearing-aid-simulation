92eb2489ee79f874727001368384fb9d73a83542
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 09:46:49 -0600

    adding frame reader factory interface

--- src/audio-stream-processing/AudioFrameReader.h
+++ src/audio-stream-processing/AudioFrameReader.h
@@ -8,3 +8,12 @@ public:
 	virtual void read(float **channels, int frameCount) = 0;
 };
 
+#include <memory>
+#include <string>
+
+class AudioFrameReaderFactory {
+public:
+	INTERFACE_OPERATIONS(AudioFrameReaderFactory);
+	virtual std::shared_ptr<AudioFrameReader> make(std::string filePath) = 0;
+};
+
