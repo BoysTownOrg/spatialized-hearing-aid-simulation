cd9ecf5d4fa4c9073203874dba74f88c6e13460e
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 11:11:23 -0600

    compile fail

--- src/google-tests/ScalingProcessorTests.cpp
+++ src/google-tests/ScalingProcessorTests.cpp
@@ -3,5 +3,8 @@
 class ScalingProcessorTestCase : public ::testing::TestCase {};
 
 TEST(ScalingProcessorTestCase, tbd) {
-	FAIL();
+	ScalingProcessor processor{ 0.5 };
+	std::vector<float> x{ 1, 2, 3 };
+	processor.process(&x[0], 3);
+	assertEqual({ 1 * 0.5, 2 * 0.5, 3 * 0.5 }, x);
 }
