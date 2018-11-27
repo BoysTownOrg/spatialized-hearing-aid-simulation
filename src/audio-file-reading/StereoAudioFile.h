49c25f040df877206c316f150236389399c4b099
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 10:02:05 -0600

    adding factory for stereo audio file

--- src/audio-file-reading/StereoAudioFile.h
+++ src/audio-file-reading/StereoAudioFile.h
@@ -7,3 +7,12 @@ public:
 	INTERFACE_OPERATIONS(StereoAudioFile);
 	virtual void read(float *left, float *right, int samples) = 0;
 };
+
+#include "AudioFileReader.h"
+#include <memory>
+
+class StereoAudioFileFactory {
+public:
+	INTERFACE_OPERATIONS(StereoAudioFileFactory);
+	virtual std::shared_ptr<StereoAudioFile> make(AudioFileReader &) = 0;
+};
