8656df7586f054547b390b36549d45b4c49429eb
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 11:31:03 -0600

    compile fail

--- src/google-tests/AudioFileAdapterTests.cpp
+++ src/google-tests/AudioFileAdapterTests.cpp
@@ -3,5 +3,15 @@
 class AudioFileAdapterTestCase : public ::testing::TestCase {};
 
 TEST(AudioFileAdapterTestCase, tbd) {
-	FAIL();
+	MockAudioFileReader reader{ { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } };
+	reader.setChannels(3);
+	AudioFileAdapter adapter{ reader };
+	std::vector<float> a(3);
+	std::vector<float> b(3);
+	std::vector<float> c(3);
+	float *channels[] { &a[0], &b[0], &c[0] };
+	adapter.read(channels, 4);
+	assertEqual({ 1, 4, 7, 10 }, a);
+	assertEqual({ 2, 5, 8, 11 }, b);
+	assertEqual({ 3, 6, 9, 12 }, c);
 }
