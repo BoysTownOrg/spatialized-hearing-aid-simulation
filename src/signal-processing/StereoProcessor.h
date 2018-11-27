2f1a0d9d551bf04b9689d9cbae546c45f650a996
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 13:22:43 -0600

    likewise removing stereo processing

--- src/signal-processing/StereoProcessor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <common-includes/Interface.h>
-
-class StereoProcessor {
-public:
-	INTERFACE_OPERATIONS(StereoProcessor);
-	virtual void process(float *xLeft, float *xRight, int samples) = 0;
-};
