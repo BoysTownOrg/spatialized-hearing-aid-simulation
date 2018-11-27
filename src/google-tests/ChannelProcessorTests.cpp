adac8a4223b0004de23ed95c0473db43daf9115b
Bashford, Seth <seth.bashford@boystown.org>
2018-11-27 10:29:14 -0600

    compile fail

--- src/google-tests/ChannelProcessorTests.cpp
+++ src/google-tests/ChannelProcessorTests.cpp
@@ -3,5 +3,14 @@
 class ChannelProcessorTestCase : public ::testing::TestCase {};
 
 TEST(ChannelProcessorTestCase, tbd) {
-	FAIL();
+	std::vector<MockSignalProcessor> processors(3);
+	ChannelProcessingGroup group{ processors };
+	float a{};
+	float b{};
+	float c{};
+	float *channels[] = { &a, &b, &c };
+	group->process(channels, 0);
+	EXPECT_EQ(&a, processors[0]->x());
+	EXPECT_EQ(&b, processors[1]->x());
+	EXPECT_EQ(&c, processors[2]->x());
 }
