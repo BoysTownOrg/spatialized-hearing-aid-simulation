510bc5a17859d84475e403429d4bbb7c4c957ad2
Bashford, Seth <seth.bashford@boystown.org>
2018-11-28 14:59:52 -0600

    second iteration compile fail

--- src/google-tests/AudioPlayerAdapterTests.cpp
+++ src/google-tests/AudioPlayerAdapterTests.cpp
@@ -4,7 +4,12 @@
 class AudioPlayerAdapterTestCase : public ::testing::TestCase {};
 
 TEST(AudioPlayerAdapterTestCase, tbd) {
-	AudioPlayerAdapterFactory adapter{};
+	const auto factory = std::make_shared<MockAudioDeviceFactory>();
+	AudioPlayerAdapterFactory adapter{ factory };
 	AudioPlayer::Parameters p{};
+	p.forAudioDevice.framesPerBuffer = 1;
+	p.forAudioDevice.sampleRate = 2;
 	adapter.make(p);
+	EXPECT_EQ(1, factory->framesPerBuffer());
+	EXPECT_EQ(2, factory->sampleRate());
 }
