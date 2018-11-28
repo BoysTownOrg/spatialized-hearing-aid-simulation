be45ee535646c96ba557807d1c9fd0de3be85b7a
Bashford, Seth <seth.bashford@boystown.org>
2018-11-28 16:48:44 -0600

    refactor

--- src/playing-audio/AudioPlayerModel.h
+++ src/playing-audio/AudioPlayerModel.h
@@ -1,34 +1,7 @@
 #pragma once
 
-#include <common-includes/Interface.h>
-#include <string>
-
-class SpatializedHearingAidSimulator {
-public:
-	struct Parameters {
-		std::string leftDslPrescriptionFilePath;
-		std::string rightDslPrescriptionFilePath;
-		std::string brirFilePath;
-		std::string audioFilePath;
-		double level_dB_Spl;
-		double attack_ms;
-		double release_ms;
-		int windowSize;
-		int chunkSize;
-	};
-	INTERFACE_OPERATIONS(SpatializedHearingAidSimulator);
-};
-
-#include <memory>
-
-class SpatializedHearingAidSimulatorFactory {
-public:
-	INTERFACE_OPERATIONS(SpatializedHearingAidSimulatorFactory);
-	virtual std::shared_ptr<SpatializedHearingAidSimulator> make(
-		SpatializedHearingAidSimulator::Parameters) = 0;
-};
-
 #include "playing-audio-exports.h"
+#include "SpatializedHearingAidSimulator.h"
 #include <presentation/SpatializedHearingAidSimulationModel.h>
 #include <audio-device-control/AudioDevice.h>
 
