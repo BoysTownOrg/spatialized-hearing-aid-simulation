b683918141c5b79a44f409d728d51726798b1ac2
Bashford, Seth <seth.bashford@boystown.org>
2018-11-28 13:04:19 -0600

    Revert "might be guessing...but I think this is what we'll need"
    This reverts commit 89a20dde5bbb2f5213898a3894decd59714ecd6a.

--- src/audio-stream-processing/AudioFrameProcessor.h
+++ src/audio-stream-processing/AudioFrameProcessor.h
@@ -5,25 +5,6 @@
 
 class AudioFrameProcessor {
 public:
-	struct Parameters {
-		std::string leftDslPrescriptionFilePath;
-		std::string rightDslPrescriptionFilePath;
-		std::string brirFilePath;
-		double level_dB_Spl;
-		double attack_ms;
-		double release_ms;
-		int windowSize;
-		int chunkSize;
-		int sampleRate;
-	};
 	INTERFACE_OPERATIONS(AudioFrameProcessor);
 	virtual void process(float **channels, int frameCount) = 0;
-};
-
-#include <memory>
-
-class AudioFrameProcessorFactory {
-public:
-	INTERFACE_OPERATIONS(AudioFrameProcessorFactory);
-	virtual std::shared_ptr<AudioFrameProcessor> make(AudioFrameProcessor::Parameters) = 0;
 };
