9e788fabae1b2d26b6998cda8c89dc71faaf79d6
Bashford, Seth <seth.bashford@boystown.org>
2018-11-30 10:08:11 -0600

    run fail

--- src/google-tests/ChannelCopierTester.cpp
+++ src/google-tests/ChannelCopierTester.cpp
@@ -1,11 +1,31 @@
+#include <audio-stream-processing/AudioFrameReader.h>
+
+class ChannelCopier : public AudioFrameReader {
+	std::shared_ptr<AudioFrameReader> reader;
+public:
+	explicit ChannelCopier(
+		std::shared_ptr<AudioFrameReader> reader
+	) :
+		reader{ std::move(reader) } {}
+
+	void read(float ** channels, int frameCount) override
+	{
+		channels;
+		frameCount;
+	}
+};
+
+#include "assert-utility.h"
+#include "MockAudioFileReader.h"
+#include <audio-file-reading/AudioFileInMemory.h>
 #include <gtest/gtest.h>
 
 class ChannelCopierTestCase : public ::testing::TestCase {};
 
 TEST(ChannelCopierTestCase, tbd) {
-	MockAudioFileReader reader{ { 1, 2, 3 } };
+	const auto reader = std::make_shared<MockAudioFileReader>(std::vector<float>{ 1, 2, 3 });
 	reader->setChannels(1);
-	AudioFileInMemory adapter{ reader };
+	const auto adapter = std::make_shared<AudioFileInMemory>(*reader);
 	ChannelCopier copier{ adapter };
 	std::vector<float> left(3);
 	std::vector<float> right(3);
