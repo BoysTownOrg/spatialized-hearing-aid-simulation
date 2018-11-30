c460520395b41b0f803d23762723238dd313aa83
Bashford, Seth <seth.bashford@boystown.org>
2018-11-30 11:09:53 -0600

    compile fail

--- src/google-tests/BrirConfigTester.cpp
+++ src/google-tests/BrirConfigTester.cpp
@@ -1,7 +1,13 @@
+#include "MockConfigurationFileParser.h"
 #include <gtest/gtest.h>
 
 class BinauralRoomImpulseResponseTester : public ::testing::TestCase {};
 
 TEST(BinauralRoomImpulseResponseTester, tbd) {
-	FAIL();
+	MockConfigurationFileParser parser{};
+	using brir_config::Property;
+	parser.setVectorProperty(propertyName(Property::leftImpulseResponse), { 1, 2, 3 });
+	parser.setVectorProperty(propertyName(Property::rightImpulseResponse), { 4, 5, 6 });
+	parser.setIntProperty(propertyName(Property::sampleRate), 7);
+	BinauralRoomImpulseResponse response{ parser };
 }
