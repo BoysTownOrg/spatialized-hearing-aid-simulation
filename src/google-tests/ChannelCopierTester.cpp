b51595c0ae3e1e8efb977887a52bf5996192d1d4
Bashford, Seth <seth.bashford@boystown.org>
2018-11-30 10:03:22 -0600

    compile fail

--- src/google-tests/ChannelCopierTester.cpp
+++ src/google-tests/ChannelCopierTester.cpp
@@ -3,5 +3,14 @@
 class ChannelCopierTestCase : public ::testing::TestCase {};
 
 TEST(ChannelCopierTestCase, tbd) {
-	FAIL();
+	MockAudioFileReader reader{ { 1, 2, 3 } };
+	reader->setChannels(1);
+	AudioFileInMemory adapter{ reader };
+	ChannelCopier copier{ adapter };
+	std::vector<float> left(3);
+	std::vector<float> right(3);
+	float *x[]{ &left[0], &right[0] };
+	copier.read(x, 3);
+	assertEqual({ 1, 2, 3 }, left);
+	assertEqual({ 1, 2, 3 }, right);
 }
