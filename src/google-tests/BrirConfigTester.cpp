28f585f2d0a2de380e8aae93e8cecdf443a71eea
Bashford, Seth <seth.bashford@boystown.org>
2018-11-30 11:17:43 -0600

    refactor and next compile fail

--- src/google-tests/BrirConfigTester.cpp
+++ src/google-tests/BrirConfigTester.cpp
@@ -1,14 +1,17 @@
+#include "assert-utility.h"
 #include "MockConfigurationFileParser.h"
+#include <binaural-room-impulse-response-config/BrirConfig.h>
 #include <gtest/gtest.h>
 
 class BinauralRoomImpulseResponseTester : public ::testing::TestCase {};
 
-TEST(BinauralRoomImpulseResponseTester, tbd) {
+TEST(BinauralRoomImpulseResponseTester, returnsValuesParsed) {
 	MockConfigurationFileParser parser{};
 	using brir_config::Property;
 	parser.setVectorProperty(propertyName(Property::leftImpulseResponse), { 1, 2, 3 });
 	parser.setVectorProperty(propertyName(Property::rightImpulseResponse), { 4, 5, 6 });
 	parser.setIntProperty(propertyName(Property::sampleRate), 7);
 	BinauralRoomImpulseResponse response{ parser };
+	assertEqual({ 1, 2, 3 }, response.left());
 	EXPECT_EQ(7, response.sampleRate());
 }
