aeadf1f7776c9d0185044beec4c6a9231a606cad
Bashford, Seth <seth.bashford@boystown.org>
2018-12-03 15:55:35 -0600

    trying different things with this profile

--- src/fir-filtering-profiling/main.cpp
+++ src/fir-filtering-profiling/main.cpp
@@ -1,7 +1,8 @@
 #include <fir-filtering/FirFilter.h>
 
 int main() {
-	FirFilter filter{ std::vector<float>{ 1, 2, 3, 4, 5 } };
-	std::vector<float> x(100000000);
-	filter.process(&x[0], 100000000);
+	FirFilter filter{ std::vector<float>(10000) };
+	std::vector<float> x(1024);
+	for (int i = 0; i < 3520; ++i)
+		filter.process(&x[0], 64);
 }
