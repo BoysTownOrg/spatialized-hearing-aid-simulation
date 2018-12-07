1fef6e56a2b229f1d029353d1752bc5b102e8acd
Bashford, Seth <seth.bashford@boystown.org>
2018-12-07 11:02:14 -0600

    fail

--- src/google-tests/BrirAdapterTests.cpp
+++ src/google-tests/BrirAdapterTests.cpp
@@ -3,5 +3,9 @@
 class BrirAdapterTestCase : public ::testing::TestCase {};
 
 TEST(BrirAdapterTestCase, tbd) {
-	FAIL();
+	const auto reader = std::make_shared<FakeAudioFileReader>({ 1, 2, 3, 4 });
+	reader->setChannels(2);
+	reader->setSampleRate(5);
+	BrirAdapter adapter{ *reader };
+	EXPECT_EQ(5, adapter.asInt(propertyName(binaural_room_impulse_response::Property::sampleRate)));
 }
