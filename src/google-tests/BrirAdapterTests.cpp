feb34cf193287ad5d7c0d9fd1a21fb95b0266dea
Bashford, Seth <seth.bashford@boystown.org>
2018-12-07 11:06:54 -0600

    run fail

--- src/google-tests/BrirAdapterTests.cpp
+++ src/google-tests/BrirAdapterTests.cpp
@@ -1,11 +1,33 @@
+#include <audio-file-reading/AudioFileReader.h>
+#include <dsl-prescription/ConfigurationFileParser.h>
+
+class BrirAdapter : public ConfigurationFileParser {
+public:
+	explicit BrirAdapter(AudioFileReader &) {}
+
+	std::vector<double> asVector(std::string ) const override {
+		return std::vector<double>();
+	}
+
+	double asDouble(std::string ) const override {
+		return 0.0;
+	}
+
+	int asInt(std::string ) const override {
+		return 0;
+	}
+};
+
+#include "FakeAudioFileReader.h"
+#include <binaural-room-impulse-response/BinauralRoomImpulseResponse.h>
 #include <gtest/gtest.h>
 
 class BrirAdapterTestCase : public ::testing::TestCase {};
 
 TEST(BrirAdapterTestCase, tbd) {
-	const auto reader = std::make_shared<FakeAudioFileReader>({ 1, 2, 3, 4 });
-	reader->setChannels(2);
-	reader->setSampleRate(5);
-	BrirAdapter adapter{ *reader };
+	FakeAudioFileReader reader{ { 1, 2, 3, 4 } };
+	reader.setChannels(2);
+	reader.setSampleRate(5);
+	BrirAdapter adapter{ reader };
 	EXPECT_EQ(5, adapter.asInt(propertyName(binaural_room_impulse_response::Property::sampleRate)));
 }
