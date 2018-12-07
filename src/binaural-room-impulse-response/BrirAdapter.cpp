745a421ae73b034e1d313c644e1325f9e96cce09
Bashford, Seth <seth.bashford@boystown.org>
2018-12-07 14:12:24 -0600

    pass

--- src/binaural-room-impulse-response/BrirAdapter.cpp
+++ src/binaural-room-impulse-response/BrirAdapter.cpp
@@ -8,11 +8,14 @@ BrirAdapter::BrirAdapter(std::shared_ptr<AudioFileReaderFactory> factory) :
 }
 
 BrirReader::BinauralRoomImpulseResponse BrirAdapter::read(std::string filePath) {
-	BinauralRoomImpulseResponse brir{};
 	const auto reader = factory->make(filePath);
+	if (reader->failed())
+		throw ReadError{ reader->errorMessage() };
+	BinauralRoomImpulseResponse brir{};
 	using vector_type = decltype(brir.left);
 	vector_type buffer(gsl::narrow<vector_type::size_type>(reader->frames() * reader->channels()));
-	reader->readFrames(&buffer[0], reader->frames());
+	if (buffer.size())
+		reader->readFrames(&buffer[0], reader->frames());
 	bool oddSample = false;
 	std::partition_copy(
 		buffer.begin(),
