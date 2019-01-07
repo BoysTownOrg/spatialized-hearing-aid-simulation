bba7a7f05bb8f90b115348bd01a9dfa66ac73c7b
Bashford, Seth <seth.bashford@boystown.org>
2019-01-07 11:25:57 -0600

    refactor

--- src/presentation/View.h
+++ src/presentation/View.h
@@ -1,9 +1,18 @@
 #pragma once
 
-#include "EventListener.h"
 #include <common-includes/Interface.h>
 #include <string>
 #include <vector>
+
+class EventListener {
+public:
+	INTERFACE_OPERATIONS(EventListener);
+	virtual void browseForLeftDslPrescription() = 0;
+	virtual void browseForRightDslPrescription() = 0;
+	virtual void browseForAudio() = 0;
+	virtual void browseForBrir() = 0;
+	virtual void play() = 0;
+};
 
 class View {
 public:
