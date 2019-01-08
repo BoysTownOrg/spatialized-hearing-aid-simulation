db5e4b79e8c15f6cdee68f70f5bc9072b7666418
Bashford, Seth <seth.bashford@boystown.org>
2019-01-08 13:55:57 -0600

    run fail

---
+++ src/google-tests/AudioPlayerTests.cpp
@@ -0,0 +1,24 @@
+#include <playing-audio/AudioDevice.h>
+
+class AudioPlayer : public AudioDeviceController {
+public:
+	AudioPlayer(AudioDevice *) {}
+
+	void fillStreamBuffer(void * channels, int frames) override {
+		channels;
+		frames;
+	}
+};
+
+#include "AudioDeviceStub.h"
+#include <gtest/gtest.h>
+
+class AudioPlayerTests : public ::testing::Test {
+protected:
+	AudioDeviceStub device{};
+	AudioPlayer player{ &device };
+};
+
+TEST_F(AudioPlayerTests, constructorSetsItselfAsDeviceController) {
+	EXPECT_EQ(&player, device.controller());
+}
