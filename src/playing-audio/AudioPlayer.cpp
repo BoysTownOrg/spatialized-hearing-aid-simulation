eb72f8ca4f8b6400a25a24ba5a92b3f3f2b7c2a3
Bashford, Seth <seth.bashford@boystown.org>
2019-01-08 14:00:34 -0600

    implementing stimulus player

--- src/playing-audio/AudioPlayer.cpp
+++ src/playing-audio/AudioPlayer.cpp
@@ -8,3 +8,8 @@ void AudioPlayer::fillStreamBuffer(void * channels, int frames) {
 	channels;
 	frames;
 }
+
+void AudioPlayer::play(std::string filePath)
+{
+	filePath;
+}
--- src/playing-audio/AudioPlayer.h
+++ src/playing-audio/AudioPlayer.h
@@ -2,10 +2,12 @@
 
 #include "AudioDevice.h"
 #include "playing-audio-exports.h"
+#include "RecognitionTestModel.h"
 
-class AudioPlayer : public AudioDeviceController {
+class AudioPlayer : public StimulusPlayer, public AudioDeviceController {
 public:
 	PLAYING_AUDIO_API AudioPlayer(AudioDevice *device);
 	void fillStreamBuffer(void * channels, int frames) override;
+	void play(std::string filePath) override;
 };
 
