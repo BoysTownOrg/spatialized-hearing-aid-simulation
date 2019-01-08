dbf56ef39297b01b1e1ba98aa1f171d9106e2f79
Bashford, Seth <seth.bashford@boystown.org>
2019-01-08 15:15:16 -0600

    pass

--- src/playing-audio/AudioPlayer.cpp
+++ src/playing-audio/AudioPlayer.cpp
@@ -23,17 +23,38 @@ void AudioPlayer::fillStreamBuffer(void * channels, int frames) {
 	frameReader->read(audio);
 }
 
-void AudioPlayer::play(PlayRequest request)
-{
-	request;
+void AudioPlayer::play(PlayRequest request) {
 	if (device->failed())
 		throw DeviceFailure{ device->errorMessage() };
+	
 	if (device->streaming())
 		return;
-	device->closeStream();
-	device->openStream({});
-	device->startStream();
-	frameReader = readerFactory->make({});
+	
+	frameReader = readerFactory->make(request.audioFilePath);
 	audio.resize(frameReader->channels());
-	frameProcessor = processorFactory->make({});
+
+	AudioFrameProcessorFactory::Parameters forProcessor;
+	forProcessor.attack_ms = request.attack_ms;
+	forProcessor.release_ms = request.release_ms;
+	forProcessor.channels = frameReader->channels();
+	forProcessor.brirFilePath = request.brirFilePath;
+	forProcessor.leftDslPrescriptionFilePath = request.leftDslPrescriptionFilePath;
+	forProcessor.rightDslPrescriptionFilePath = request.rightDslPrescriptionFilePath;
+	forProcessor.level_dB_Spl = request.level_dB_Spl;
+	forProcessor.sampleRate = frameReader->sampleRate();
+	forProcessor.chunkSize = request.chunkSize;
+	forProcessor.windowSize = request.windowSize;
+	frameProcessor = processorFactory->make(forProcessor);
+
+	AudioDevice::StreamParameters forStreaming;
+	forStreaming.sampleRate = frameReader->sampleRate();
+	forStreaming.channels = frameReader->channels();
+	forStreaming.framesPerBuffer = request.chunkSize;
+	for (int i = 0; i < device->count(); ++i)
+		if (device->description(i) == request.audioDevice)
+			forStreaming.deviceIndex = i;
+
+	device->closeStream();
+	device->openStream(forStreaming);
+	device->startStream();
 }
