69bda437766dd16d3f9f20cf3ff6d16e6b34a234
Bashford, Seth <seth.bashford@boystown.org>
2019-01-08 15:40:54 -0600

    refactor

--- src/playing-audio/RecognitionTestModel.cpp
+++ src/playing-audio/RecognitionTestModel.cpp
@@ -28,15 +28,6 @@ bool RecognitionTestModel::testComplete() {
 }
 
 void RecognitionTestModel::initializeTest(TestParameters p) {
-	AudioFrameProcessorFactory::Parameters forProcessor;
-	forProcessor.attack_ms = p.attack_ms;
-	forProcessor.release_ms = p.release_ms;
-	forProcessor.brirFilePath = p.brirFilePath;
-	forProcessor.leftDslPrescriptionFilePath = p.leftDslPrescriptionFilePath;
-	forProcessor.rightDslPrescriptionFilePath = p.rightDslPrescriptionFilePath;
-	forProcessor.level_dB_Spl = p.level_dB_Spl;
-	forProcessor.chunkSize = p.chunkSize;
-	forProcessor.windowSize = p.windowSize;
 	list->initialize(p.audioDirectory);
 }
 
