4b54a401deff452ee31ef6c809bd93dc707d0da1
Bashford, Seth <seth.bashford@boystown.org>
2019-01-08 14:01:56 -0600

    run fail

--- src/google-tests/AudioPlayerTests.cpp
+++ src/google-tests/AudioPlayerTests.cpp
@@ -1,5 +1,6 @@
 #include <playing-audio/AudioPlayer.h>
 #include "AudioDeviceStub.h"
+#include "assert-utility.h"
 #include <gtest/gtest.h>
 
 class AudioPlayerTests : public ::testing::Test {
@@ -10,4 +11,9 @@ protected:
 
 TEST_F(AudioPlayerTests, constructorSetsItselfAsDeviceController) {
 	EXPECT_EQ(&player, device.controller());
+}
+
+TEST_F(AudioPlayerTests, playFirstClosesStreamThenOpensThenStarts) {
+	player.play({});
+	assertEqual("close open start ", device.streamLog());
 }
