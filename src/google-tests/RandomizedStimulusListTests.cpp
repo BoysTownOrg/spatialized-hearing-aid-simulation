c40583a02605c6ceaa73acd66e32631e2739825b
Bashford, Seth <seth.bashford@boystown.org>
2019-01-09 11:20:01 -0600

    pass of course

--- src/google-tests/RandomizedStimulusListTests.cpp
+++ src/google-tests/RandomizedStimulusListTests.cpp
@@ -1,3 +1,84 @@
+#include <recognition-test/RecognitionTestModel.h>
+#include <common-includes/Interface.h>
+
+class DirectoryReader {
+public:
+    INTERFACE_OPERATIONS(DirectoryReader);
+    virtual std::vector<std::string> filesIn(std::string directory) = 0;
+};
+    
+class Randomizer {
+public:
+    INTERFACE_OPERATIONS(Randomizer);
+    using shuffle_iterator = std::vector<std::string>::iterator;
+    virtual void shuffle(shuffle_iterator begin, shuffle_iterator end) = 0;
+};
+
+class RandomizedStimulusList : public StimulusList {
+    std::vector<std::string> files{};
+    std::string directory_{};
+    DirectoryReader *reader;
+    Randomizer *randomizer;
+public:
+    RandomizedStimulusList(DirectoryReader *reader, Randomizer *randomizer);
+    void initialize(std::string directory) override;
+    bool empty() override;
+    std::string next() override;
+};
+
+RandomizedStimulusList::RandomizedStimulusList(
+    DirectoryReader *reader,
+    Randomizer *randomizer
+) :
+    reader{reader},
+    randomizer{randomizer} {}
+    
+void RandomizedStimulusList::initialize(std::string directory) {
+    directory_ = std::move(directory);
+    files = reader->filesIn(directory_);
+    randomizer->shuffle(files.begin(), files.end());
+}
+    
+bool RandomizedStimulusList::empty() {
+    return files.empty();
+}
+    
+std::string RandomizedStimulusList::next() {
+    auto next_ = files.front();
+    files.erase(files.begin());
+    return directory_ + "/" + next_;
+}
+
+class FileFilterDecorator : public DirectoryReader {
+    std::string filter;
+    DirectoryReader *reader;
+public:
+    FileFilterDecorator(DirectoryReader *reader, std::string filter);
+    std::vector<std::string> filesIn(std::string directory) override;
+private:
+    std::vector<std::string> filtered(std::vector<std::string>);
+};
+
+FileFilterDecorator::FileFilterDecorator(
+    DirectoryReader *reader,
+    std::string filter
+) :
+    filter{filter},
+    reader{reader} {}
+
+std::vector<std::string> FileFilterDecorator::filesIn(std::string directory) {
+    return filtered(reader->filesIn(directory));
+}
+    
+std::vector<std::string> FileFilterDecorator::filtered(std::vector<std::string> files) {
+    std::vector<std::string> filtered_{};
+    for (auto &file : files)
+        if (file.length() >= filter.length())
+            if (0 == file.compare(file.length() - filter.length(), filter.length(), filter))
+                filtered_.push_back(file);
+    return filtered_;
+}
+
 #include "assert-utility.h"
 #include <gtest/gtest.h>
 
