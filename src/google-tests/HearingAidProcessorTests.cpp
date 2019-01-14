42116ef64784f70a78afdb29093bbf101b69049e
Bashford, Seth <seth.bashford@boystown.org>
2019-01-14 17:33:12 -0600

    refactor

--- src/google-tests/HearingAidProcessorTests.cpp
+++ src/google-tests/HearingAidProcessorTests.cpp
@@ -3,15 +3,11 @@
 #include <hearing-aid-processing/HearingAidProcessor.h>
 #include <gtest/gtest.h>
 
-class HearingAidProcessorFacade {
-	HearingAidProcessor processor;
-	std::shared_ptr<FilterbankCompressor> compressor;
-public:
-	explicit HearingAidProcessorFacade(
-		std::shared_ptr<FilterbankCompressor> compressor
-	) :
-		processor{ compressor },
-		compressor{ compressor } {}
+class HearingAidProcessorTests : public ::testing::Test {
+protected:
+	std::shared_ptr<FilterbankCompressorSpy> compressor =
+		std::make_shared<FilterbankCompressorSpy>();
+	HearingAidProcessor processor{ compressor };
 
 	void processUnequalChunk() {
 		std::vector<float> x(compressor->chunkSize() + 1);
@@ -23,55 +19,38 @@ public:
 		_process(x);
 	}
 
-	void process(gsl::span<float> x) {
-		_process(x);
-	}
-
 private:
 	void _process(gsl::span<float> x) {
 		processor.process(x);
 	}
 };
 
-class HearingAidProcessorTestCase : public ::testing::TestCase {};
-
-TEST(
-	HearingAidProcessorTestCase,
-	failedCompressorThrowsCompressorError)
-{
-	try {
-		const auto compressor = std::make_shared<FilterbankCompressorSpy>();
-		compressor->fail();
-		HearingAidProcessorFacade processor{ compressor };
-		FAIL() << "Expected HearingAidProcessor::CompressorError";
-	}
-	catch (const HearingAidProcessor::CompressorError &e) {
-		assertEqual("The compressor failed to initialize.", e.what());
-	}
-}
-
-TEST(
-	HearingAidProcessorTestCase, 
-	processCallsFilterbankCompressorMethodsInCorrectOrder) 
-{
-	const auto compressor = std::make_shared<FilterbankCompressorSpy>();
-	HearingAidProcessorFacade processor{ compressor };
-	processor.process();
+TEST_F(
+	HearingAidProcessorTests, 
+	processCallsFilterbankCompressorMethodsInCorrectOrder
+) {
+	process();
 	assertEqual(
 		"compressInput"
 		"analyzeFilterbank"
 		"compressChannels"
 		"synthesizeFilterbank"
 		"compressOutput",
-		compressor->processingLog());
+		compressor->processingLog()
+	);
 }
 
-TEST(HearingAidProcessorTestCase, processPassesChunkSize)
-{
-	const auto compressor = std::make_shared<FilterbankCompressorSpy>();
+TEST_F(
+	HearingAidProcessorTests,
+	processDoesNotCallCompressorWhenFrameCountDoesNotEqualChunkSize
+) {
+	processUnequalChunk();
+	EXPECT_TRUE(compressor->processingLog().empty());
+}
+
+TEST_F(HearingAidProcessorTests, processPassesChunkSize) {
 	compressor->setChunkSize(1);
-	HearingAidProcessorFacade processor{ compressor };
-	processor.process();
+	process();
 	EXPECT_EQ(1, compressor->compressInputChunkSize());
 	EXPECT_EQ(1, compressor->filterbankAnalyzeChunkSize());
 	EXPECT_EQ(1, compressor->compressChannelsChunkSize());
@@ -80,13 +59,18 @@ TEST(HearingAidProcessorTestCase, processPassesChunkSize)
 }
 
 TEST(
-	HearingAidProcessorTestCase,
-	processDoesNotCallCompressorWhenFrameCountDoesNotEqualChunkSize)
-{
-	const auto compressor = std::make_shared<FilterbankCompressorSpy>();
-	HearingAidProcessorFacade processor{ compressor };
-	processor.processUnequalChunk();
-	EXPECT_TRUE(compressor->processingLog().empty());
+	HearingAidProcessorOtherTests,
+	failedCompressorThrowsCompressorError
+) {
+	try {
+		const auto compressor = std::make_shared<FilterbankCompressorSpy>();
+		compressor->fail();
+		HearingAidProcessor processor{ compressor };
+		FAIL() << "Expected HearingAidProcessor::CompressorError";
+	}
+	catch (const HearingAidProcessor::CompressorError &e) {
+		assertEqual("The compressor failed to initialize.", e.what());
+	}
 }
 
 class MultipliesRealSignalsByPrimes : public FilterbankCompressor {
@@ -125,11 +109,11 @@ public:
 };
 
 TEST(
-	HearingAidProcessorTestCase,
-	processPassesRealInputsAppropriately)
-{
+	HearingAidProcessorOtherTests,
+	processPassesRealInputsAppropriately
+) {
 	const auto compressor = std::make_shared<MultipliesRealSignalsByPrimes>();
-	HearingAidProcessorFacade processor{ compressor };
+	HearingAidProcessor processor{ compressor };
 	std::vector<float> x = { 4 };
 	processor.process(x);
 	assertEqual({ 4 * 2 * 3 * 5 * 7 * 11 * 13 }, x);
@@ -189,22 +173,53 @@ public:
 	}
 };
 
+class HearingAidProcessorFacade {
+	HearingAidProcessor processor;
+	std::shared_ptr<FilterbankCompressor> compressor;
+public:
+	explicit HearingAidProcessorFacade(
+		std::shared_ptr<FilterbankCompressor> compressor
+	) :
+		processor{ compressor },
+		compressor{ compressor } {}
+
+	void processUnequalChunk() {
+		std::vector<float> x(compressor->chunkSize() + 1);
+		_process(x);
+	}
+
+	void process() {
+		std::vector<float> x(compressor->chunkSize());
+		_process(x);
+	}
+
+	void process(gsl::span<float> x) {
+		_process(x);
+	}
+
+private:
+	void _process(gsl::span<float> x) {
+		processor.process(x);
+	}
+};
+
 TEST(
-	HearingAidProcessorTestCase,
-	processPassesComplexInputsAppropriately)
-{
+	HearingAidProcessorOtherTests,
+	processPassesComplexInputsAppropriately
+) {
 	const auto compressor = std::make_shared<ForComplexSignalTests>();
 	HearingAidProcessorFacade processor{ compressor };
 	processor.process();
 	EXPECT_EQ(
 		(0 + 7) * 11 * 13 * 17 * 19,
-		compressor->postSynthesizeFilterbankComplexResult());
+		compressor->postSynthesizeFilterbankComplexResult()
+	);
 }
 
 TEST(
-	HearingAidProcessorTestCase,
-	complexInputSizeIsAtLeastChannelTimesChunkSizeTimesTwo)
-{
+	HearingAidProcessorOtherTests,
+	complexInputSizeIsAtLeastChannelTimesChunkSizeTimesTwo
+) {
 	const auto compressor = std::make_shared<ForComplexSignalTests>();
 	compressor->setChunkSize(3);
 	compressor->setChannels(5);
@@ -213,5 +228,6 @@ TEST(
 	processor.process();
 	EXPECT_EQ(
 		(0 + 7) * 11 * 13 * 17 * 19,
-		compressor->postSynthesizeFilterbankComplexResult());
+		compressor->postSynthesizeFilterbankComplexResult()
+	);
 }
