435adf50edc05400796d53200740117cb049ae6b
Bashford, Seth <seth.bashford@boystown.org>
2019-01-15 16:23:41 -0600

    refactor

--- src/google-tests/ChannelProcessingGroupTests.cpp
+++ src/google-tests/ChannelProcessingGroupTests.cpp
@@ -2,15 +2,17 @@
 #include <signal-processing/ChannelProcessingGroup.h>
 #include <gtest/gtest.h>
 
-class ChannelProcessingGroupTestCase : public ::testing::TestCase {};
-
-TEST(ChannelProcessingGroupTestCase, processesChannelsInOrder) {
+class ChannelProcessingGroupTests : public ::testing::Test {
+protected:
 	std::vector<std::shared_ptr<SignalProcessorStub>> processors{
 		std::make_shared<SignalProcessorStub>(),
 		std::make_shared<SignalProcessorStub>(),
 		std::make_shared<SignalProcessorStub>()
 	};
-	ChannelProcessingGroup group{ { processors[0], processors[1], processors[2] } };
+	ChannelProcessingGroup group{ { processors.begin(), processors.end() } };
+};
+
+TEST_F(ChannelProcessingGroupTests, processesChannelsInOrder) {
 	gsl::span<float> a{};
 	gsl::span<float> b{};
 	gsl::span<float> c{};
@@ -21,18 +23,9 @@ TEST(ChannelProcessingGroupTestCase, processesChannelsInOrder) {
 	EXPECT_EQ(c, processors[2]->signal());
 }
 
-TEST(ChannelProcessingGroupTestCase, groupDelayReturnsMaxGroupDelay) {
-	auto processor1 = std::make_shared<SignalProcessorStub>();
-	processor1->setGroupDelay(1);
-	auto processor2 = std::make_shared<SignalProcessorStub>();
-	processor2->setGroupDelay(2);
-	auto processor3 = std::make_shared<SignalProcessorStub>();
-	processor3->setGroupDelay(3);
-	std::vector<std::shared_ptr<SignalProcessorStub>> processors{
-		processor1,
-		processor2,
-		processor3
-	};
-	ChannelProcessingGroup group{ { processors[0], processors[1], processors[2] } };
+TEST_F(ChannelProcessingGroupTests, groupDelayReturnsMaxGroupDelay) {
+	processors[0]->setGroupDelay(1);
+	processors[1]->setGroupDelay(2);
+	processors[2]->setGroupDelay(3);
 	EXPECT_EQ(3, group.groupDelay());
 }
