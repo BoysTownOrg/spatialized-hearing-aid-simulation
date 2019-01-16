d738d87f8029b9fcd62b33f9b71a84c3d60b32d3
Bashford, Seth <seth.bashford@boystown.org>
2019-01-16 16:41:21 -0600

    run fail

--- src/google-tests/RefactoredAudioFrameProcessorImplTests.cpp
+++ src/google-tests/RefactoredAudioFrameProcessorImplTests.cpp
@@ -36,45 +36,84 @@ class RefactoredAudioFrameProcessorImplFactory{};
 #include "AudioFrameProcessorStub.h"
 #include <gtest/gtest.h>
 
-class RefactoredAudioFrameProcessorImplTests : public ::testing::Test {
-protected:
-	AudioFrameReaderStub reader{};
-	AudioFrameProcessorStub processor{};
-	RefactoredAudioFrameProcessorImpl impl{&reader, &processor};
-};
+namespace {
+	class RefactoredAudioFrameProcessorImplTests : public ::testing::Test {
+	protected:
+		AudioFrameReaderStub reader{};
+		AudioFrameProcessorStub processor{};
+		RefactoredAudioFrameProcessorImpl impl{ &reader, &processor };
+	};
 
-TEST_F(RefactoredAudioFrameProcessorImplTests, processPassesAudioToReaderAndProcessor) {
-	gsl::span<float> x{};
-	impl.process({ &x, 1 });
-	EXPECT_EQ(&x, reader.audioBuffer().data());
-	EXPECT_EQ(1, reader.audioBuffer().size());
-	EXPECT_EQ(&x, processor.audioBuffer().data());
-	EXPECT_EQ(1, processor.audioBuffer().size());
-}
+	TEST_F(RefactoredAudioFrameProcessorImplTests, processPassesAudioToReaderAndProcessor) {
+		gsl::span<float> x{};
+		impl.process({ &x, 1 });
+		EXPECT_EQ(&x, reader.audioBuffer().data());
+		EXPECT_EQ(1, reader.audioBuffer().size());
+		EXPECT_EQ(&x, processor.audioBuffer().data());
+		EXPECT_EQ(1, processor.audioBuffer().size());
+	}
 
-TEST_F(RefactoredAudioFrameProcessorImplTests, processPadsZeroToEndOfInput) {
-	reader.setComplete();
-	std::vector<float> audio(3, -1);
-	gsl::span<float> x{ audio };
-	impl.process({ &x, 1 });
-	EXPECT_EQ(0, processor.audioBuffer()[0][0]);
-	EXPECT_EQ(0, processor.audioBuffer()[0][1]);
-	EXPECT_EQ(0, processor.audioBuffer()[0][2]);
-}
+	TEST_F(RefactoredAudioFrameProcessorImplTests, processPadsZeroToEndOfInput) {
+		reader.setComplete();
+		std::vector<float> audio(3, -1);
+		gsl::span<float> x{ audio };
+		impl.process({ &x, 1 });
+		EXPECT_EQ(0, processor.audioBuffer()[0][0]);
+		EXPECT_EQ(0, processor.audioBuffer()[0][1]);
+		EXPECT_EQ(0, processor.audioBuffer()[0][2]);
+	}
 
-TEST_F(RefactoredAudioFrameProcessorImplTests, completeAfterProcessingPaddedZeroes) {
-	reader.setComplete();
-	processor.setGroupDelay(3);
-	float y{};
-	gsl::span<float> x{ &y, 1 };
-	impl.process({ &x, 1 });
-	EXPECT_FALSE(impl.complete());
-	impl.process({ &x, 1 });
-	EXPECT_FALSE(impl.complete());
-	impl.process({ &x, 1 });
-	EXPECT_TRUE(impl.complete());
-}
+	TEST_F(RefactoredAudioFrameProcessorImplTests, completeAfterProcessingPaddedZeroes) {
+		reader.setComplete();
+		processor.setGroupDelay(3);
+		float y{};
+		gsl::span<float> x{ &y, 1 };
+		impl.process({ &x, 1 });
+		EXPECT_FALSE(impl.complete());
+		impl.process({ &x, 1 });
+		EXPECT_FALSE(impl.complete());
+		impl.process({ &x, 1 });
+		EXPECT_TRUE(impl.complete());
+	}
 
-TEST(RefactoredAudioFrameProcessorOtherTests, tbd2) {
-	RefactoredAudioFrameProcessorImplFactory factory{};
+	class ReadsAOne : public AudioFrameReader {
+		void read(gsl::span<gsl::span<float>> audio) override {
+			for (const auto channel : audio)
+				for (auto &x : channel)
+					x = 1;
+		}
+
+		int channels() const override {
+			return 1;
+		}
+
+		bool complete() const override { return {}; }
+		int sampleRate() const override { return {}; }
+		long long frames() const override { return {}; }
+		void reset() override {}
+	};
+
+	class TimesTwo : public AudioFrameProcessor {
+		void process(gsl::span<gsl::span<float>> audio) override {
+			for (const auto channel : audio)
+				for (auto &x : channel)
+					x *= 2;
+		}
+
+		int groupDelay() override { return {}; }
+	};
+
+	TEST(RefactoredAudioFrameProcessorOtherTests, processReadsThenProcesses) {
+		ReadsAOne reader{};
+		TimesTwo processor{};
+		RefactoredAudioFrameProcessorImpl impl{ &reader, &processor };
+		float y{};
+		gsl::span<float> x{ &y, 1 };
+		impl.process({ &x, 1 });
+		EXPECT_EQ(2, y);
+	}
+
+	TEST(RefactoredAudioFrameProcessorOtherTests, tbd2) {
+		RefactoredAudioFrameProcessorImplFactory factory{};
+	}
 }
