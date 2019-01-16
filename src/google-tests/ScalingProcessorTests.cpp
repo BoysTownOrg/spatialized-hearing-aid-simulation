706a4e9863f6ff1364032337884805cdbddaa7d4
Bashford, Seth <seth.bashford@boystown.org>
2019-01-16 14:45:16 -0600

    refactor

--- src/google-tests/ScalingProcessorTests.cpp
+++ src/google-tests/ScalingProcessorTests.cpp
@@ -2,16 +2,14 @@
 #include <signal-processing/ScalingProcessor.h>
 #include <gtest/gtest.h>
 
-class ScalingProcessorTestCase : public ::testing::TestCase {};
-
-TEST(ScalingProcessorTestCase, processScalesAccordingToScalar) {
+TEST(ScalingProcessorTests, processScalesAccordingToScalar) {
 	ScalingProcessor processor{ 0.5 };
 	std::vector<float> x{ 1, 2, 3 };
 	processor.process({ x });
 	assertEqual({ 1 * 0.5, 2 * 0.5, 3 * 0.5 }, x);
 }
 
-TEST(ScalingProcessorTestCase, groupDelayReturnsZero) {
+TEST(ScalingProcessorTests, groupDelayReturnsZero) {
 	ScalingProcessor processor{ {} };
 	EXPECT_EQ(0, processor.groupDelay());
 };
--- src/google-tests/SignalProcessingChainTests.cpp
+++ src/google-tests/SignalProcessingChainTests.cpp
@@ -19,44 +19,44 @@ namespace {
 			for (auto &x : signal)
 				x *= 2;
 		}
-		
+
 		int groupDelay() override { return {}; }
 	};
-}
 
-class SignalProcessingChainTests : public ::testing::Test {
-protected:
-	SignalProcessingChain chain{};
-	std::shared_ptr<SignalProcessorStub> processor = 
-		std::make_shared<SignalProcessorStub>();
+	class SignalProcessingChainTests : public ::testing::Test {
+	protected:
+		SignalProcessingChain chain{};
+		std::shared_ptr<SignalProcessorStub> processor =
+			std::make_shared<SignalProcessorStub>();
 
-	SignalProcessingChainTests() {
-		chain.add(processor);
+		SignalProcessingChainTests() {
+			chain.add(processor);
+		}
+	};
+
+	TEST_F(SignalProcessingChainTests, chainCallsProcessorsInOrder) {
+		chain.add(std::make_shared<AddOne>());
+		chain.add(std::make_shared<TimesTwo>());
+		float x = 1;
+		chain.process({ &x, 1 });
+		EXPECT_EQ(4, x);
 	}
-};
 
-TEST_F(SignalProcessingChainTests, chainCallsProcessorsInOrder) {
-	chain.add(std::make_shared<AddOne>());
-	chain.add(std::make_shared<TimesTwo>());
-	float x = 1;
-	chain.process({ &x, 1 });
-	EXPECT_EQ(4, x);
-}
+	TEST_F(SignalProcessingChainTests, chainPassesParametersToProcessor) {
+		float x{};
+		const gsl::span<float> channel{ &x, 1 };
+		chain.process(channel);
+		EXPECT_EQ(channel, processor->signal());
+	}
 
-TEST_F(SignalProcessingChainTests, chainPassesParametersToProcessor) {
-	float x{};
-	const gsl::span<float> channel{ &x, 1 };
-	chain.process(channel);
-	EXPECT_EQ(channel, processor->signal());
-}
-
-TEST_F(SignalProcessingChainTests, groupDelayReturnsSumOfComponents) {
-	processor->setGroupDelay(1);
-	auto processor2 = std::make_shared<SignalProcessorStub>();
-	processor2->setGroupDelay(2);
-	chain.add(processor2);
-	auto processor3 = std::make_shared<SignalProcessorStub>();
-	processor3->setGroupDelay(3);
-	chain.add(processor3);
-	EXPECT_EQ(1 + 2 + 3, chain.groupDelay());
+	TEST_F(SignalProcessingChainTests, groupDelayReturnsSumOfComponents) {
+		processor->setGroupDelay(1);
+		auto processor2 = std::make_shared<SignalProcessorStub>();
+		processor2->setGroupDelay(2);
+		chain.add(processor2);
+		auto processor3 = std::make_shared<SignalProcessorStub>();
+		processor3->setGroupDelay(3);
+		chain.add(processor3);
+		EXPECT_EQ(1 + 2 + 3, chain.groupDelay());
+	}
 }
