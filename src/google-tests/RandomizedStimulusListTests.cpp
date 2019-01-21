3f98ed913ad59f93c6aff94175c94c3585b92368
Bashford, Seth <seth.bashford@boystown.org>
2019-01-21 15:46:33 -0600

    refactor

--- src/google-tests/RandomizedStimulusListTests.cpp
+++ src/google-tests/RandomizedStimulusListTests.cpp
@@ -33,81 +33,82 @@ public:
     }
 };
 
-class RandomizedStimulusListTests : public ::testing::Test {
-protected:
-    DirectoryReaderStub reader{};
-    RandomizerStub randomizer{};
-    RandomizedStimulusList list{&reader, &randomizer};
-};
+namespace {
+	class RandomizedStimulusListTests : public ::testing::Test {
+	protected:
+		DirectoryReaderStub reader{};
+		RandomizerStub randomizer{};
+		RandomizedStimulusList list{ &reader, &randomizer };
+	};
 
-TEST_F(
-    RandomizedStimulusListTests,
-    initializePassesDirectoryToDirectoryReader
-) {
-    list.initialize("a");
-    assertEqual("a", reader.directory());
+	TEST_F(
+		RandomizedStimulusListTests,
+		initializePassesDirectoryToDirectoryReader
+	) {
+		list.initialize("a");
+		assertEqual("a", reader.directory());
+	}
+
+	TEST_F(
+		RandomizedStimulusListTests,
+		emptyWhenEmpty
+	) {
+		reader.setFileNames({ "a", "b", "c" });
+		list.initialize({});
+		EXPECT_FALSE(list.empty());
+		list.next();
+		EXPECT_FALSE(list.empty());
+		list.next();
+		EXPECT_FALSE(list.empty());
+		list.next();
+		EXPECT_TRUE(list.empty());
+	}
+
+	TEST_F(
+		RandomizedStimulusListTests,
+		nextReturnsFullPathToFileAtFront
+	) {
+		reader.setFileNames({ "a", "b", "c" });
+		list.initialize({ "C:" });
+		assertEqual("C:/a", list.next());
+		assertEqual("C:/b", list.next());
+		assertEqual("C:/c", list.next());
+	}
+
+	TEST_F(
+		RandomizedStimulusListTests,
+		nextReturnsFullPathToLastFileWhenExhausted
+	) {
+		reader.setFileNames({ "a", "b", "c" });
+		list.initialize({ "C:" });
+		list.next();
+		list.next();
+		list.next();
+		assertEqual("C:/c", list.next());
+		assertEqual("C:/c", list.next());
+		assertEqual("C:/c", list.next());
+	}
+
+	TEST_F(
+		RandomizedStimulusListTests,
+		initializeShufflesFileNames
+	) {
+		reader.setFileNames({ "a", "b", "c" });
+		list.initialize({});
+		assertEqual({ "a", "b", "c" }, randomizer.toShuffle());
+	}
+
+	TEST(FileFilterDecoratorTests, passesDirectoryToDecorated) {
+		DirectoryReaderStub reader;
+		FileFilterDecorator decorator{ &reader, {} };
+		decorator.filesIn({ "a" });
+		assertEqual("a", reader.directory());
+	}
+
+	TEST(FileFilterDecoratorTests, returnsFilteredFiles) {
+		DirectoryReaderStub reader;
+		FileFilterDecorator decorator{ &reader, ".c" };
+		reader.setFileNames({ "a", "b.c", "d.e", "f.c", "g.h" });
+		assertEqual({ "b.c", "f.c" }, decorator.filesIn({}));
+	}
 }
-
-TEST_F(
-    RandomizedStimulusListTests,
-    emptyWhenEmpty
-) {
-    reader.setFileNames({ "a", "b", "c" });
-    list.initialize({});
-    EXPECT_FALSE(list.empty());
-    list.next();
-    EXPECT_FALSE(list.empty());
-    list.next();
-    EXPECT_FALSE(list.empty());
-    list.next();
-    EXPECT_TRUE(list.empty());
-}
-
-TEST_F(
-    RandomizedStimulusListTests,
-    nextReturnsFullPathToFileAtFront
-) {
-    reader.setFileNames({ "a", "b", "c" });
-    list.initialize({"C:"});
-    assertEqual("C:/a", list.next());
-    assertEqual("C:/b", list.next());
-    assertEqual("C:/c", list.next());
-}
-
-TEST_F(
-    RandomizedStimulusListTests,
-    nextReturnsFullPathToLastFileWhenExhausted
-) {
-    reader.setFileNames({ "a", "b", "c" });
-    list.initialize({"C:"});
-	list.next();
-	list.next();
-	list.next();
-    assertEqual("C:/c", list.next());
-    assertEqual("C:/c", list.next());
-    assertEqual("C:/c", list.next());
-}
-
-TEST_F(
-    RandomizedStimulusListTests,
-    initializeShufflesFileNames
-) {
-    reader.setFileNames({ "a", "b", "c" });
-    list.initialize({});
-    assertEqual({ "a", "b", "c" }, randomizer.toShuffle());
-}
-
-TEST(FileFilterDecoratorTests, passesDirectoryToDecorated) {
-    DirectoryReaderStub reader;
-    FileFilterDecorator decorator{&reader, {}};
-    decorator.filesIn({"a"});
-    assertEqual("a", reader.directory());
-}
-
-TEST(FileFilterDecoratorTests, returnsFilteredFiles) {
-    DirectoryReaderStub reader;
-    FileFilterDecorator decorator{&reader, ".c"};
-    reader.setFileNames({ "a", "b.c", "d.e", "f.c", "g.h" });
-    assertEqual({ "b.c", "f.c" }, decorator.filesIn({}));
-}
-
