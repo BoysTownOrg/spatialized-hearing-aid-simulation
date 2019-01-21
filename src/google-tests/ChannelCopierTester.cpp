54f78c7c738c1c98633c4c5846f447efea347bfe
Bashford, Seth <seth.bashford@boystown.org>
2019-01-21 16:07:14 -0600

    refactor

--- src/google-tests/ChannelCopierTester.cpp
+++ src/google-tests/ChannelCopierTester.cpp
@@ -5,55 +5,58 @@
 #include <audio-stream-processing/ChannelCopier.h>
 #include <gtest/gtest.h>
 
-class ChannelCopierTests : public ::testing::Test {
-protected:
-	std::shared_ptr<AudioFrameReaderStub> reader =
-		std::make_shared<AudioFrameReaderStub>();
-	ChannelCopier copier{ reader };
-};
+namespace {
+	class ChannelCopierTests : public ::testing::Test {
+	protected:
+		std::shared_ptr<AudioFrameReaderStub> reader =
+			std::make_shared<AudioFrameReaderStub>();
+		ChannelCopier copier{ reader };
+	};
 
-TEST_F(ChannelCopierTests, returnsCompleteWhenComplete) {
-	EXPECT_FALSE(copier.complete());
-	reader->setComplete();
-	EXPECT_TRUE(copier.complete());
-}
+	TEST_F(ChannelCopierTests, returnsCompleteWhenComplete) {
+		EXPECT_FALSE(copier.complete());
+		reader->setComplete();
+		EXPECT_TRUE(copier.complete());
+	}
 
-TEST_F(ChannelCopierTests, returnsParameters) {
-	reader->setSampleRate(1);
-	reader->setChannels(2);
-	reader->setFrames(3);
-    reader->setRemainingFrames(4);
-	EXPECT_EQ(1, copier.sampleRate());
-	EXPECT_EQ(2, copier.channels());
-	EXPECT_EQ(3, copier.frames());
-    EXPECT_EQ(4, copier.framesRemaining());
-}
+	TEST_F(ChannelCopierTests, returnsParameters) {
+		reader->setSampleRate(1);
+		reader->setFrames(2);
+		reader->setChannels(3);
+		reader->setRemainingFrames(4);
+		EXPECT_EQ(1, copier.sampleRate());
+		EXPECT_EQ(2, copier.frames());
+		EXPECT_EQ(3, copier.channels());
+		EXPECT_EQ(4, copier.framesRemaining());
+	}
 
-TEST_F(ChannelCopierTests, returnsTwoIfOneChannel) {
-	reader->setChannels(1);
-	EXPECT_EQ(2, copier.channels());
-}
+	TEST_F(ChannelCopierTests, returnsTwoIfOneChannel) {
+		reader->setChannels(1);
+		EXPECT_EQ(2, copier.channels());
+	}
 
-TEST_F(ChannelCopierTests, reset) {
-	copier.reset();
-	EXPECT_TRUE(reader->readingLog().contains("reset "));
-}
+	TEST_F(ChannelCopierTests, reset) {
+		copier.reset();
+		EXPECT_TRUE(reader->readingLog().contains("reset "));
+	}
 
-TEST(ChannelCopierOtherTests, copiesFirstChannelToSecondWhenOnlyOneChannel) {
-	FakeAudioFileReader reader;
-	reader.setContents({ 1, 2, 3 });
-	ChannelCopier copier{ std::make_shared<AudioFileInMemory>(reader) };
-	std::vector<float> left(3);
-	std::vector<float> right(3);
-	std::vector<gsl::span<float>> x{ left, right };
-	copier.read(x);
-	assertEqual({ 1, 2, 3 }, left);
-	assertEqual({ 1, 2, 3 }, right);
-}
+	TEST(ChannelCopierOtherTests, copiesFirstChannelToSecondWhenOnlyOneChannel) {
+		FakeAudioFileReader reader;
+		reader.setContents({ 1, 2, 3 });
+		reader.setChannels(1);
+		ChannelCopier copier{ std::make_shared<AudioFileInMemory>(reader) };
+		std::vector<float> left(3);
+		std::vector<float> right(3);
+		std::vector<gsl::span<float>> stereo{ left, right };
+		copier.read(stereo);
+		assertEqual({ 1, 2, 3 }, left);
+		assertEqual({ 1, 2, 3 }, right);
+	}
 
-TEST(ChannelCopierOtherTests, factoryPassesFilePath) {
-	const auto factory = std::make_shared<AudioFrameReaderStubFactory>();
-	ChannelCopierFactory adapter{ factory };
-	adapter.make("a");
-	assertEqual("a", factory->filePath());
+	TEST(ChannelCopierOtherTests, factoryPassesFilePath) {
+		const auto factory = std::make_shared<AudioFrameReaderStubFactory>();
+		ChannelCopierFactory adapter{ factory };
+		adapter.make("a");
+		assertEqual("a", factory->filePath());
+	}
 }
