f2d4d31a1266945baec2c6544b6f4ab9b8d7d20f
Bashford, Seth <seth.bashford@boystown.org>
2019-01-21 15:37:05 -0600

    refactor

--- src/google-tests/ScalingProcessorTests.cpp
+++ src/google-tests/ScalingProcessorTests.cpp
@@ -5,7 +5,7 @@
 TEST(ScalingProcessorTests, processScalesAccordingToScalar) {
 	ScalingProcessor processor{ 0.5 };
 	std::vector<float> x{ 1, 2, 3 };
-	processor.process({ x });
+	processor.process(x);
 	assertEqual({ 1 * 0.5, 2 * 0.5, 3 * 0.5 }, x);
 }
 
