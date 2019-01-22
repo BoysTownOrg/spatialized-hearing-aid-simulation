27c4beb134d26cf464707b08484cb8af2ab61a05
Bashford, Seth <seth.bashford@boystown.org>
2019-01-22 12:30:54 -0600

    implementing in main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -72,12 +72,21 @@ class FileSystemDocumenter : public Documenter {
 	std::ofstream file;
 public:
 	void initialize(std::string filePath) override {
-		file.open(filePath);
+		file.open(std::move(filePath));
 	}
 
 	void write(std::string s) override {
-		file << s;
-		file << '\n';
+		file << std::move(s);
+	}
+
+	bool failed() override {
+		return file.fail();
+	}
+
+	std::string errorMessage() override {
+		char buffer[256];
+		strerror_s(buffer, sizeof buffer, errno);
+		return buffer;
 	}
 };
 
