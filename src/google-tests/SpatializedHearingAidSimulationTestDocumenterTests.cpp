d8df39b1bb44c7631b00a4a4a677084508e7a087
Bashford, Seth <seth.bashford@boystown.org>
2019-01-25 09:52:33 -0600

    run fail

--- src/google-tests/SpatializedHearingAidSimulationTestDocumenterTests.cpp
+++ src/google-tests/SpatializedHearingAidSimulationTestDocumenterTests.cpp
@@ -43,6 +43,36 @@ TEST_F(
 
 TEST_F(
 	SpatializedHearingAidSimulationTestDocumenterTests,
+	ignoresPrescriptionsIfNotUsingHearingAidSimulation
+) {
+	GlobalTestParameters test;
+	test.subjectId = "a";
+	test.testerId = "b";
+	test.leftDslPrescriptionFilePath = "c";
+	test.rightDslPrescriptionFilePath = "d";
+	test.brirFilePath = "e";
+	test.attack_ms = 1;
+	test.release_ms = 2;
+	test.windowSize = 3;
+	test.chunkSize = 4;
+	test.usingHearingAidSimulation = false;
+	SpatializedHearingAidSimulationTestDocumenter::TestParameters p;
+	p.global = &test;
+	documenter.documentTestParameters(p);
+	assertEqual(
+		"subject: a\n"
+		"tester: b\n"
+		"BRIR: e\n"
+		"attack (ms): 1.0\n"
+		"release (ms): 2.0\n"
+		"window size (samples): 3\n"
+		"chunk size (samples): 4\n\n", 
+		writer.content()
+	);
+}
+
+TEST_F(
+	SpatializedHearingAidSimulationTestDocumenterTests,
 	playTrialDocumentsTrial
 ) {
 	GlobalTrialParameters trial;
