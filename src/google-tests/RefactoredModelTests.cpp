6a27a3ab2a0ee1cc1b4b61bafe185d1ae1f34d3c
Bashford, Seth <seth.bashford@boystown.org>
2019-01-31 13:57:54 -0600

    fail

--- src/google-tests/RefactoredModelTests.cpp
+++ src/google-tests/RefactoredModelTests.cpp
@@ -496,21 +496,17 @@ namespace {
 	}
 
 	TEST_F(RefactoredModelTests, playTrialLoadsLoaderWithProcessor) {
-		testParameters.usingSpatialization = true;
-		testParameters.usingHearingAidSimulation = true;
 		audioFrameReader->setChannels(2);
 		prepareNewTest();
-		scalarFactory.setProcessor(std::make_shared<AddsSamplesBy>(1.0f));
-		firFilterFactory.setProcessor(std::make_shared<MultipliesSamplesBy>(2.0f));
-		hearingAidFactory.setProcessor(std::make_shared <AddsSamplesBy>(3.0f));
+		simulationFactory.setProcessor(std::make_shared<AddsSamplesBy>(1.0f));
 		playTrial();
 		auto processor = audioLoader.audioFrameProcessor();
 		std::vector<float> left{ 4 };
 		std::vector<float> right{ 5 };
 		std::vector<gsl::span<float>> channels{ left, right };
 		processor->process(channels);
-		EXPECT_EQ((4 + 1) * 2 + 3, left.at(0));
-		EXPECT_EQ((5 + 1) * 2 + 3, right.at(0));
+		EXPECT_EQ(4 + 1, left.front());
+		EXPECT_EQ(5 + 1, right.front());
 	}
 
 	TEST_F(RefactoredModelTests, playTrialNoSpatialization) {
