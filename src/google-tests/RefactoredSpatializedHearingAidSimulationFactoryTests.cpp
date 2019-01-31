acede89dd00b1fa5828be185c8fe917e21bee1db
Bashford, Seth <seth.bashford@boystown.org>
2019-01-31 12:37:05 -0600

    run fail

--- src/google-tests/RefactoredSpatializedHearingAidSimulationFactoryTests.cpp
+++ src/google-tests/RefactoredSpatializedHearingAidSimulationFactoryTests.cpp
@@ -1,14 +1,49 @@
-#include <gtest/gtest.h>
+#include <signal-processing/SignalProcessor.h>
+#include <spatialized-hearing-aid-simulation/RefactoredModel.h>
 
-class RefactoredSpatializedHearingAidSimulationFactoryTests : public ::testing::Test {
-protected:
+class RefactoredSpatializedHearingAidSimulationFactory {
+public:
+	RefactoredSpatializedHearingAidSimulationFactory(ScalarFactory *) {}
+
+	struct SimulationParameters {
+		float scale;
+	};
+	std::shared_ptr<SignalProcessor> make(SimulationParameters) {
+		return {};
+	}
 };
 
+#include <gtest/gtest.h>
 
+namespace {
+	class ScalarFactoryStub : public ScalarFactory {
+		float scalar_{};
+		std::shared_ptr<SignalProcessor> processor{};
+	public:
+		void setProcessor(std::shared_ptr<SignalProcessor> p) noexcept {
+			processor = std::move(p);
+		}
 
-TEST_F(RefactoredSpatializedHearingAidSimulationFactoryTests, makePassesScalarToFactory) {
-	SimulationParameters p;
-	p.scale = 1;
-	simulationFactory.make(p);
-	EXPECT_EQ(1, scalarFactory.scalars().at(0));
+		auto scalar() const {
+			return scalar_;
+		}
+
+		std::shared_ptr<SignalProcessor> make(float x) override {
+			scalar_ = x;
+			return processor;
+		}
+	};
+
+	class RefactoredSpatializedHearingAidSimulationFactoryTests : public ::testing::Test {
+	protected:
+		ScalarFactoryStub scalarFactory{};
+		RefactoredSpatializedHearingAidSimulationFactory simulationFactory{ &scalarFactory };
+	};
+
+	TEST_F(RefactoredSpatializedHearingAidSimulationFactoryTests, makePassesScalarToFactory) {
+		RefactoredSpatializedHearingAidSimulationFactory::SimulationParameters p;
+		p.scale = 1;
+		simulationFactory.make(p);
+		EXPECT_EQ(1, scalarFactory.scalar());
+	}
 }
