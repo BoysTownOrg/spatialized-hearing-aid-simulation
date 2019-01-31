73f9c411cd47d6e70e0b39e4ff6cbe33cad55934
Bashford, Seth <seth.bashford@boystown.org>
2019-01-31 09:37:05 -0600

    fail

--- src/google-tests/RefactoredModelTests.cpp
+++ src/google-tests/RefactoredModelTests.cpp
@@ -297,9 +297,13 @@ TEST_F(RefactoredModelTests, prepareNewTestDoesNotReadBrirWhenNotUsingSpatializa
 TEST_F(RefactoredModelTests, prepareNewTestPassesParametersToSpeechPerceptionTest) {
 	testParameters.audioDirectory = "a";
 	testParameters.testFilePath = "b";
+	testParameters.subjectId = "c";
+	testParameters.testerId = "d";
 	prepareNewTest();
 	assertEqual("a", perceptionTest.testParameters().audioDirectory);
 	assertEqual("b", perceptionTest.testParameters().testFilePath);
+	assertEqual("c", perceptionTest.testParameters().subjectId);
+	assertEqual("d", perceptionTest.testParameters().testerId);
 }
 
 TEST_F(RefactoredModelTests, playTrialPassesStimulusPlayerToSpeechPerceptionTest) {
