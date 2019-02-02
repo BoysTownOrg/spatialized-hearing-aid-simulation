7c2b7bbc60bf8eedcbfeeaa788ed44998be62389
sbash64 <sbash64@gmail.com>
2019-02-02 13:36:12 -0600

    compile fail

--- src/google-tests/SpatializedHearingAidSimulationFactoryTests.cpp
+++ src/google-tests/SpatializedHearingAidSimulationFactoryTests.cpp
@@ -188,4 +188,17 @@ namespace {
 		processor->process(x);
 		assertEqual({ (4 + 1) * 2 + 3.0f }, x);
 	}
+
+	TEST_F(
+		SpatializedHearingAidSimulationFactoryTests,
+		makeHearingAidSimulationCombinesProcessorsInOrder
+	) {
+		scalarFactory.setProcessor(std::make_shared<AddsSamplesBy>(1.0f));
+		firFilterFactory.setProcessor(std::make_shared<MultipliesSamplesBy>(2.0f));
+		hearingAidFactory.setProcessor(std::make_shared<AddsSamplesBy>(3.0f));
+		auto processor = simulationFactory.makeHearingAidSimulation(simulationParameters);
+		buffer_type x{ 4 };
+		processor->process(x);
+		assertEqual({ 4 + 1 + 3.0f }, x);
+	}
 }
