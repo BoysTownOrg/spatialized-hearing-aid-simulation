eb06c8c7b5bfdde9d5e3d16f92c110d9e986b301
Bashford, Seth <seth.bashford@boystown.org>
2019-02-04 16:03:00 -0600

    refactor

--- src/spatialized-hearing-aid-simulation/CalibrationComputer.h
+++ src/spatialized-hearing-aid-simulation/CalibrationComputer.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "AudioFrameReader.h"
+#include "ICalibrationComputer.h"
 #include "spatialized-hearing-aid-simulation-exports.h"
 
-class CalibrationComputer {
+class CalibrationComputer : public ICalibrationComputer {
 	using sample_type = AudioFrameReader::channel_type::element_type;
 	using channel_type = std::vector<sample_type>;
     std::vector<channel_type> audioFileContents;
 public:
 	SPATIALIZED_HA_SIMULATION_API explicit CalibrationComputer(AudioFrameReader &reader);
-	SPATIALIZED_HA_SIMULATION_API double signalScale(int channel, double level);
+	SPATIALIZED_HA_SIMULATION_API double signalScale(int channel, double level) override;
 
 private:
 	void read(AudioFrameReader & reader);
---
+++ src/spatialized-hearing-aid-simulation/ICalibrationComputer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "AudioFrameReader.h"
+#include <common-includes/Interface.h>
+#include <memory>
+
+class ICalibrationComputer {
+public:
+	INTERFACE_OPERATIONS(ICalibrationComputer);
+	virtual double signalScale(int channel, double level) = 0;
+};
+
+class ICalibrationComputerFactory {
+public:
+	INTERFACE_OPERATIONS(ICalibrationComputerFactory);
+	virtual std::shared_ptr<ICalibrationComputer> make(AudioFrameReader &) = 0;
+};
