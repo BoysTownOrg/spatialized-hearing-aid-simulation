a61d9218bacdf79ff5bbb2a15a9ce97ae4846c6f
Bashford, Seth <seth.bashford@boystown.org>
2019-02-04 16:00:15 -0600

    refactor

--- src/spatialized-hearing-aid-simulation/CalibrationComputer.h
+++ src/spatialized-hearing-aid-simulation/CalibrationComputer.h
@@ -1,44 +1,17 @@
 #pragma once
 
 #include "AudioFrameReader.h"
+#include "spatialized-hearing-aid-simulation-exports.h"
 
 class CalibrationComputer {
 	using sample_type = AudioFrameReader::channel_type::element_type;
 	using channel_type = std::vector<sample_type>;
     std::vector<channel_type> audioFileContents;
 public:
-	explicit CalibrationComputer(AudioFrameReader &reader) :
-		audioFileContents(
-			reader.channels(), 
-			channel_type(gsl::narrow<channel_type::size_type>(reader.frames()))
-		)
-	{
-		read(reader);
-	}
-
-	double signalScale(int channel, double level) {
-		return std::pow(10.0, level / 20.0) / rms(audioFileContents.at(channel));
-	}
+	SPATIALIZED_HA_SIMULATION_API explicit CalibrationComputer(AudioFrameReader &reader);
+	SPATIALIZED_HA_SIMULATION_API double signalScale(int channel, double level);
 
 private:
-	void read(AudioFrameReader & reader) {
-		std::vector<AudioFrameReader::channel_type> adapted;
-		for (auto &channel : audioFileContents)
-			adapted.push_back({ channel });
-		reader.read(adapted);
-		reader.reset();
-	}
-
-	template<typename T>
-	T rms(std::vector<T> x) {
-		return std::sqrt(
-			std::accumulate(
-				x.begin(),
-				x.end(),
-				T{ 0 },
-				[](T a, T b) { return a += b * b; }
-			) / x.size()
-		);
-	}
+	void read(AudioFrameReader & reader);
 };
 
