792ee9b631b3b9bc01f488d0e10f72261884ec74
Bashford, Seth <seth.bashford@boystown.org>
2019-02-04 10:15:05 -0600

    fail

--- src/google-tests/RefactoredModelTests.cpp
+++ src/google-tests/RefactoredModelTests.cpp
@@ -273,6 +273,20 @@ namespace {
 		EXPECT_NEAR(desiredRms / rightChannelRms, simulationFactory.parameters().at(1).scale, 1e-6);
 	}
 
+	TEST_F(RefactoredModelTests, playTrialComputesCalibrationScalarsForFullSimulation) {
+		FakeAudioFileReader fakeReader{ { 1, 2, 3, 4, 5, 6 } };
+		fakeReader.setChannels(2);
+		setInMemoryReader(fakeReader);
+		trialParameters.level_dB_Spl = 7;
+		playTrial();
+		const auto desiredRms = 
+			std::pow(10.0, (7 - RefactoredModel::fullScaleLevel_dB_Spl) / 20.0);
+		const auto leftChannelRms = std::sqrt((1 * 1 + 3 * 3 + 5 * 5.0) / 3);
+		const auto rightChannelRms = std::sqrt((2 * 2 + 4 * 4 + 6 * 6.0) / 3);
+		EXPECT_NEAR(desiredRms / leftChannelRms, simulationFactory.fullSimulationScale().at(0), 1e-6);
+		EXPECT_NEAR(desiredRms / rightChannelRms, simulationFactory.fullSimulationScale().at(1), 1e-6);
+	}
+
 	TEST_F(RefactoredModelTests, playTrialResetsReaderAfterComputingRms) {
 		playTrial();
 		assertTrue(audioFrameReader->readingLog().endsWith("reset "));
