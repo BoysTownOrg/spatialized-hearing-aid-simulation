799a6738f951e35e55c8da03c465dd3276822002
Bashford, Seth <seth.bashford@boystown.org>
2019-02-04 15:58:35 -0600

    refactor

--- src/google-tests/CalibrationComputerTests.cpp
+++ src/google-tests/CalibrationComputerTests.cpp
@@ -1,49 +1,8 @@
-#include <spatialized-hearing-aid-simulation/AudioFrameReader.h>
-
-class CalibrationComputer {
-	using sample_type = AudioFrameReader::channel_type::element_type;
-	using channel_type = std::vector<sample_type>;
-    std::vector<channel_type> audioFileContents;
-public:
-	explicit CalibrationComputer(AudioFrameReader &reader) :
-		audioFileContents(
-			reader.channels(), 
-			channel_type(gsl::narrow<channel_type::size_type>(reader.frames()))
-		)
-	{
-		read(reader);
-	}
-
-	double signalScale(int channel, double level) {
-		return std::pow(10.0, level / 20.0) / rms(audioFileContents.at(channel));
-	}
-
-private:
-	void read(AudioFrameReader & reader) {
-		std::vector<AudioFrameReader::channel_type> adapted;
-		for (auto &channel : audioFileContents)
-			adapted.push_back({ channel });
-		reader.read(adapted);
-		reader.reset();
-	}
-
-	template<typename T>
-	T rms(std::vector<T> x) {
-		return std::sqrt(
-			std::accumulate(
-				x.begin(),
-				x.end(),
-				T{ 0 },
-				[](T a, T b) { return a += b * b; }
-			) / x.size()
-		);
-	}
-};
-
 #include "FakeAudioFileReader.h"
 #include "AudioFrameReaderStub.h"
 #include "assert-utility.h"
 #include <audio-file-reading/AudioFileInMemory.h>
+#include <spatialized-hearing-aid-simulation/CalibrationComputer.h>
 #include <gtest/gtest.h>
 
 class CalibrationComputerTests : public ::testing::Test {
