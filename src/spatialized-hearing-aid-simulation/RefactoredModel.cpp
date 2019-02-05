401d9302e68dc4bee0dc3489aac7450ade1968db
Bashford, Seth <seth.bashford@boystown.org>
2019-02-05 10:32:12 -0600

    pass

--- src/spatialized-hearing-aid-simulation/RefactoredModel.cpp
+++ src/spatialized-hearing-aid-simulation/RefactoredModel.cpp
@@ -229,12 +229,18 @@ void RefactoredModel::playCalibration(CalibrationParameters p) {
 	auto right_hs = both_hs;
 	right_hs.prescription = rightPrescription_;
 
+	ISpatializedHearingAidSimulationFactory::FullSimulation left_fs;
+	left_fs.hearingAid = left_hs;
+
+	ISpatializedHearingAidSimulationFactory::FullSimulation right_fs;
+	right_fs.hearingAid = right_hs;
+
 	auto computer = calibrationFactory->make(reader.get());
 	const auto digitalLevel = p.level_dB_Spl - fullScaleLevel_dB_Spl;
 	auto left_scale = gsl::narrow_cast<float>(computer->signalScale(0, digitalLevel));
 	auto right_scale = gsl::narrow_cast<float>(computer->signalScale(1, digitalLevel));
-	simulationFactory->makeFullSimulation({}, left_scale);
-	simulationFactory->makeFullSimulation({}, right_scale);
+	simulationFactory->makeFullSimulation(left_fs, left_scale);
+	simulationFactory->makeFullSimulation(right_fs, right_scale);
 	simulationFactory->makeHearingAidSimulation(left_hs, left_scale);
 	simulationFactory->makeHearingAidSimulation(right_hs, right_scale);
 	simulationFactory->makeSpatialization({},left_scale);
