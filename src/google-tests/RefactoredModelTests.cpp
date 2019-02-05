742136eb240e91f83d7045276a86d9711b0106ee
Bashford, Seth <seth.bashford@boystown.org>
2019-02-05 09:37:21 -0600

    refactor

--- src/google-tests/RefactoredModelTests.cpp
+++ src/google-tests/RefactoredModelTests.cpp
@@ -527,13 +527,11 @@ namespace {
 	}
 
 	TEST_F(RefactoredModelTests, playCalibrationComputesCalibrationScalarsForHearingAidSimulation) {
-		audioFrameReader->setChannels(2);
-		calibrationComputer->addSignalScale(0, 3.3);
-		calibrationComputer->addSignalScale(1, 4.4);
 		setHearingAidSimulationOnly();
-		playCalibration();
-		assertEqual(3.3f, simulationFactory.hearingAidSimulationScale().at(0));
-		assertEqual(4.4f, simulationFactory.hearingAidSimulationScale().at(1));
+		assertScalarsMatchCalibrationAfterCall(
+			simulationFactory.hearingAidSimulationScale(),
+			[=]() { playCalibration(); }
+		);
 	}
 
 	TEST_F(RefactoredModelTests, playTrialComputesCalibrationScalarsForSpatialization) {
