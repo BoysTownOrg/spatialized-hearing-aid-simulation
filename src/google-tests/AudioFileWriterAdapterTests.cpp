45bf24ae2ff11798e472fc16ed9d267afc3447fd
Bashford, Seth <seth.bashford@boystown.org>
2019-02-22 12:29:45 -0600

    refactor

--- src/google-tests/AudioFileWriterAdapterTests.cpp
+++ src/google-tests/AudioFileWriterAdapterTests.cpp
@@ -19,7 +19,7 @@ public:
 	using channel_type = gsl::span<float>;
 
 	void write(gsl::span<channel_type> audio) {
-		std::vector<float> buffer{};
+		std::vector<channel_type::element_type> buffer{};
 		for (int i = 0; i < audio.begin()->size(); ++i)
 			for (auto channel : audio)
 				buffer.push_back(channel.at(i));
