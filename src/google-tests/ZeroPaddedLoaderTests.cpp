bd47d94edfeafda8b3b522e403c0d3a72f032ea0
Bashford, Seth <seth.bashford@boystown.org>
2019-02-26 17:10:04 -0600

    little better

--- src/google-tests/ZeroPaddedLoaderTests.cpp
+++ src/google-tests/ZeroPaddedLoaderTests.cpp
@@ -16,18 +16,15 @@ namespace {
 		buffer_type left{};
 		buffer_type right{};
 
-		explicit ZeroPaddedLoaderFacade(
+		ZeroPaddedLoaderFacade(
 			AudioFileReader &reader,
-			std::shared_ptr<AudioFrameProcessor> processor =
-				std::make_shared<AudioFrameProcessorStub>()
+			std::shared_ptr<AudioFrameProcessor> processor
 		) :
 			loader{ std::make_shared<AudioFileInMemory>(reader), std::move(processor) } {}
 
-		explicit ZeroPaddedLoaderFacade(
-			std::shared_ptr<AudioFrameReader> r =
-				std::make_shared<AudioFrameReaderStub>(),
-			std::shared_ptr<AudioFrameProcessor> p =
-				std::make_shared<AudioFrameProcessorStub>()
+		ZeroPaddedLoaderFacade(
+			std::shared_ptr<AudioFrameReader> r,
+			std::shared_ptr<AudioFrameProcessor> p
 		) :
 			loader{ std::move(r), std::move(p) } {}
 
@@ -55,20 +52,10 @@ namespace {
 
 	class ZeroPaddedLoaderTests : public ::testing::Test {
 	protected:
-		using channel_type = ZeroPaddedLoader::channel_type;
-		using buffer_type = std::vector<channel_type::element_type>;
-		using size_type = buffer_type::size_type;
-		buffer_type left{};
-		buffer_type right{};
 		std::shared_ptr<AudioFrameReaderStub> reader =
 			std::make_shared<AudioFrameReaderStub>();
 		std::shared_ptr<AudioFrameProcessorStub> processor =
 			std::make_shared<AudioFrameProcessorStub>();
-		ZeroPaddedLoader loader{reader, processor};
-
-		void reset() {
-			loader.reset();
-		}
 
 		ZeroPaddedLoaderFacade construct(
 			AudioFileReader &reader_, 
@@ -78,78 +65,67 @@ namespace {
 			return ZeroPaddedLoaderFacade{ reader_, std::move(p) };
 		}
 
-		ZeroPaddedLoaderFacade construct(
-			std::shared_ptr<AudioFrameProcessor> p =
-				std::make_shared<AudioFrameProcessorStub>(),
-			std::shared_ptr<AudioFrameReader> r =
-				std::make_shared<AudioFrameReaderStub>()
-		) {
-			return ZeroPaddedLoaderFacade{ std::move(r), std::move(p) };
-		}
-
-		void assertComplete() {
-			assertTrue(loader.complete());
-		}
-
-		void assertIncomplete() {
-			assertFalse(loader.complete());
+		ZeroPaddedLoaderFacade construct() {
+			return ZeroPaddedLoaderFacade{ reader, processor };
 		}
 	};
 
 	TEST_F(ZeroPaddedLoaderTests, queriesDoNotThrow) {
+		auto loader = construct();
 		assertTrue(loader.complete());
 	}
 
 	TEST_F(ZeroPaddedLoaderTests, loadPadsZeroToEndOfInputMono) {
 		FakeAudioFileReader fakeReader{ { 1, 2, 3 } };
-		auto loader_ = construct(fakeReader);
-		loader_.loadMonoFrames(4);
-		assertEqual({ 1, 2, 3, 0 }, loader_.left);
+		auto loader = construct(fakeReader);
+		loader.loadMonoFrames(4);
+		assertEqual({ 1, 2, 3, 0 }, loader.left);
 	}
 
 	TEST_F(ZeroPaddedLoaderTests, loadPadsZeroToEndOfInputStereo) {
 		FakeAudioFileReader fakeReader{ { 1, 2, 3, 4, 5, 6 } };
 		fakeReader.setChannels(2);
-		auto loader_ = construct(fakeReader);
-		loader_.loadStereoFrames(4);
-		assertEqual({ 1, 3, 5, 0 }, loader_.left);
-		assertEqual({ 2, 4, 6, 0 }, loader_.right);
+		auto loader = construct(fakeReader);
+		loader.loadStereoFrames(4);
+		assertEqual({ 1, 3, 5, 0 }, loader.left);
+		assertEqual({ 2, 4, 6, 0 }, loader.right);
 	}
 
 	TEST_F(ZeroPaddedLoaderTests, completeAfterLoadingGroupDelayManyZeros) {
 		processor->setGroupDelay(3);
-		auto loader_ = construct(processor);
-		loader_.loadMonoFrames(1);
-		assertFalse(loader_.complete());
-		loader_.loadMonoFrames(1);
-		assertFalse(loader_.complete());
-		loader_.loadMonoFrames(1);
-		assertTrue(loader_.complete());
+		auto loader = construct();
+		loader.loadMonoFrames(1);
+		assertFalse(loader.complete());
+		loader.loadMonoFrames(1);
+		assertFalse(loader.complete());
+		loader.loadMonoFrames(1);
+		assertTrue(loader.complete());
 	}
 
 	TEST_F(ZeroPaddedLoaderTests, completeAfterLoadingGroupDelayManyZerosPartiallyPaddedLoad) {
-		buffer_type tenSamples(10);
+		ZeroPaddedLoaderFacade::buffer_type tenSamples(10);
 		FakeAudioFileReader fakeReader{ tenSamples };
-		auto loader_ = construct(fakeReader, processor);
+		auto loader = construct(fakeReader, processor);
 		processor->setGroupDelay(2);
-		loader_.loadMonoFrames(10 + 1);
-		assertFalse(loader_.complete());
-		loader_.loadMonoFrames(1);
-		assertTrue(loader_.complete());
+		loader.loadMonoFrames(10 + 1);
+		assertFalse(loader.complete());
+		loader.loadMonoFrames(1);
+		assertTrue(loader.complete());
 	}
 
 	TEST_F(ZeroPaddedLoaderTests, notCompleteIfReaderStillHasFramesRemaining) {
 		reader->setRemainingFrames(1);
-		assertIncomplete();
+		auto loader = construct();
+		assertFalse(loader.complete());
 	}
 
 	TEST_F(ZeroPaddedLoaderTests, resetResetsZeroPadCount) {
 		processor->setGroupDelay(1);
-		auto loader_ = construct(processor);
-		loader_.loadMonoFrames(1);
-		assertTrue(loader_.complete());
-		loader_.reset();
-		assertFalse(loader_.complete());
+		auto loader = construct();
+		loader.loadMonoFrames(1);
+		assertTrue(loader.complete());
+		loader.reset();
+		assertFalse(loader.complete());
 	}
 
 	class TimesTwo : public AudioFrameProcessor {
@@ -164,9 +140,9 @@ namespace {
 
 	TEST_F(ZeroPaddedLoaderTests, loadReadsThenProcesses) {
 		FakeAudioFileReader fakeReader{ { 1, 2, 3 } };
-		auto loader_ = construct(fakeReader, std::make_shared<TimesTwo>());
-		loader_.loadMonoFrames(3);
-		assertEqual({ 2, 4, 6 }, loader_.left);
+		auto loader = construct(fakeReader, std::make_shared<TimesTwo>());
+		loader.loadMonoFrames(3);
+		assertEqual({ 2, 4, 6 }, loader.left);
 	}
 
 	class AddsOne : public AudioFrameProcessor {
@@ -181,8 +157,8 @@ namespace {
 
 	TEST_F(ZeroPaddedLoaderTests, loadPadsZerosBeforeProcessing) {
 		FakeAudioFileReader fakeReader{ { 1, 2, 3 } };
-		auto loader_ = construct(fakeReader, std::make_shared<AddsOne>());
-		loader_.loadMonoFrames(4);
-		assertEqual({ 1 + 1, 2 + 1, 3 + 1, 0 + 1 }, loader_.left);
+		auto loader = construct(fakeReader, std::make_shared<AddsOne>());
+		loader.loadMonoFrames(4);
+		assertEqual({ 1 + 1, 2 + 1, 3 + 1, 0 + 1 }, loader.left);
 	}
 }
