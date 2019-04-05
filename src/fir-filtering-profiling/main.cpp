64c0e0485ef77e3a2e379ca0e0d8489abb1bc4cc
Bashford, Seth <seth.bashford@boystown.org>
2019-04-05 10:20:50 -0500

    fixing some errors

--- src/fir-filtering-profiling/main.cpp
+++ src/fir-filtering-profiling/main.cpp
@@ -1,7 +1,7 @@
 #include <fir-filtering/FirFilter.h>
 
 int main() {
-	FirFilter filter{ std::vector<float>(10000) };
+	FirFilter<float> filter{ std::vector<float>(10000) };
 	std::vector<float> x(1024);
 	for (int i = 0; i < 3520; ++i)
 		filter.process(x);
